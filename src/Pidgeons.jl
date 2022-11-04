a49dd157a2f148c6ed5e76ec4d7a9548ff7f8f1b
Nikola Surjanovic <16364942+nikola-sur@users.noreply.github.com>
2022-11-04 13:00:35 -0700

    Move ParallelTempering to Pidgeons module

--- src/Pidgeons.jl
+++ src/Pidgeons.jl
@@ -1,5 +1,28 @@
 module Pidgeons
 
-# Write your package code here.
+using Base: Forward
+using Distributions
+using StatsBase
+using StatsFuns
+using ForwardDiff
+using Interpolations
+using Roots
+using Dates
 
-end
+export nrpt, DEO, computeEtas, roundtrip, restarts
+
+### Samplers
+include("samplers/slice_sample.jl")
+
+### NRPT
+include("acceptance.jl")
+include("adaptation.jl")
+include("deo.jl")
+include("exploration.jl")
+include("restarts.jl")
+include("NRPT.jl")
+
+### Utility functions
+include("utils.jl")
+
+end # End module
--- test/runtests.jl
+++ test/runtests.jl
@@ -1,11 +1,5 @@
 using Pidgeons
 using Test
-include("../src/ParallelTempering.jl") # temp
-using .ParallelTempering
-using Distributions
-# using Statistics
-# using LinearAlgebra
-# using Suppressor
 using Distributions
 using Random
 using ForwardDiff
@@ -22,7 +16,7 @@ end
     V_0(θ) = -logpdf(Normal(10.0, 1.0), θ[1])
     prior_sampler() = [rand(Normal(10.0, 1.0))]
     V_1(θ) = -logpdf(Normal(0.0, 1.0), θ[1])
-    out = ParallelTempering.NRPT(V_0, V_1, InitialState, ntotal, N, optimreference = false, prior_sampler = prior_sampler).States
+    out = Pidgeons.NRPT(V_0, V_1, InitialState, ntotal, N, optimreference = false, prior_sampler = prior_sampler).States
     final_states = map((i) -> out[i][end], 1:length(out))
     final_mean = mean(final_states)[1]
     @test (final_mean < 0.5) && (final_mean > -0.5)
