6f9d6b54b92ce9df116a88e80a9b98339786e409
Alexandre Bouchard <alexandre.bouchard@gmail.com>
2022-11-30 20:32:03 -0800

    Minor

--- test/one_per_host_test.jl
+++ test/one_per_host_test.jl
@@ -3,13 +3,19 @@ using OnlineStats
 using Random
 using MPI
 
-MPI.Init()
-rk = MPI.Comm_rank(MPI.COMM_WORLD)
-sz = MPI.Comm_size(MPI.COMM_WORLD)
-result = one_per_host(MPI.COMM_WORLD)
+function test_one_host_per_host()
+    MPI.Init()
+    rk = MPI.Comm_rank(MPI.COMM_WORLD)
+    sz = MPI.Comm_size(MPI.COMM_WORLD)
+    result = one_per_host(MPI.COMM_WORLD)
 
 
-if rk == 0
-    subsz = MPI.Comm_size(result)
-    println("Subset of $subsz out of $sz")
+    if rk == 0
+        subsz = MPI.Comm_size(result)
+        println("Subset of $subsz out of $sz")
+    end
 end
+
+test_one_host_per_host()
+
+
