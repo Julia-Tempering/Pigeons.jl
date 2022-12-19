54bda951e3d19109c9289e218c944c010b0de95c
Alexandre Bouchard <alexandre.bouchard@gmail.com>
2022-12-19 13:12:52 -0800

    A bit more doc

--- src/Immutable.jl
+++ src/Immutable.jl
@@ -28,11 +28,21 @@ To work around this space-inefficiency, `Immutable` can be used as follows:
 
 1. Enclose large data inside a `Immutable`. Use `key` to 
     distinguish between different data objects if needed. 
-2. Use `Serialization.serialize` as usual. 
+    Internally, we maintain an internal, global Dict indexed 
+    by `Val{key}` storing the data. This global Dict is 
+    called `immutables`.
+2. Use `Serialization.serialize` as usual. Internally, we 
+    dispatch serialization of `Immutable` is modified to skip 
+    the field containing the data.
 3. Make one of the processes call [`serialize_immutables()`](@ref). 
+    This serializes the `immutables` Dict.
 4. Then for de-serialization, each process should call 
-    [`deserialize_immutables()`](@ref). 
-5. Finally, call `Serialization.deserialize()` as usual.
+    [`deserialize_immutables()`](@ref). This restores 
+    `immutable`.
+5. Finally, call `Serialization.deserialize()` as usual. 
+    When an `Immutable` instances is being deserialize, we 
+    dispatch deserialization so that the data is retreived 
+    from `immutable`.
 """
 Immutable(key, data::T) where {T} = Immutable(Val{key}, data)
 
