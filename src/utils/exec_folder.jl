99205c793576cc85a0f0d3e33cc6fafdad82e9d7
Alexandre Bouchard <alexandre.bouchard@gmail.com>
2023-01-07 20:32:56 -0800

    Prep another undo

--- src/utils/exec_folder.jl
+++ src/utils/exec_folder.jl
@@ -1,9 +1,6 @@
 """
-Return a folder which is unique to this execution (process). 
-By default, the directory is a subfolder of 
-results/all/[unique_name] where the unique_name is 
-based on the current time 
-and the default random number generator. 
+Return a unique subfolder of 
+`results/all/`.  
 
 In some cases it is useful to have a parent 
 process (e.g. job submission script) pick 
@@ -11,36 +8,31 @@ this unique exec folder for us; to do so, specify
 the unique name in the environment 
 variable EXEC_DIR. If the ENV variable 
 EXEC_DIR is defined, 
-this function will return its contents.
+this function will return its contents, and then 
+clear the value of that key to ensure subsequent 
+calls to `next_exec_folder()` preserve its contract.
 
 Either way, this function will also make sure the 
 unique folder and its parents are created. 
 It will also create a soft symlink to it 
 called `results/latest``
 """
-function exec_folder()
-    if exec_dir[] === nothing
-        next_exec_folder()
-    end
-    return exec_dir[]
-end
-const exec_dir = Ref{Union{Nothing,String}}(nothing)
-const used_env = Ref(false)
-
-"""
-Force the creation of a new global exec_dir. 
-"""
 function next_exec_folder()
-    exec_dir[] = if haskey(ENV, "EXEC_DIR") && !used_env[]
-        used_env[] = true
-        ENV["EXEC_DIR"]
+    result = if haskey(ENV, "EXEC_DIR") && !used_env[]
+        pop!(ENV, "EXEC_DIR")
     else
         formatted_time = Dates.format(now(), dateformat"yyyy-mm-dd-HH-MM-SS")
         "results/all/$formatted_time-$(randstring(8))"
     end
-    mkpath(exec_dir[])
-    _ensure_symlinked(exec_dir[])
-    return exec_dir[]
+    mkpath(result)
+    _ensure_symlinked(result)
+    return result
+end
+
+function pop!(dict::Dict, key)
+    result = dict[key]
+    delete!(dict, key)
+    return result 
 end
 
 """
