153f1f574947df020e501bdc41f2b2fdfafc8baa
Nikola Surjanovic <16364942+nikola-sur@users.noreply.github.com>
2023-01-14 11:19:26 -0800

    Fix bug with negative log_potential in sampler

--- test/slice_sample_test.jl
+++ test/slice_sample_test.jl
@@ -2,7 +2,7 @@ using Pigeons
 using Distributions
 using BenchmarkTools
 
-log_potential = (x) -> -logpdf(Normal(0.0, 1.0), x[1])
+log_potential = (x) -> logpdf(Normal(0.0, 1.0), x[1])
 h = Pigeons.SliceSampler()
 state = [0.3]
 
--- test/slice_sample_test_2.jl
+++ test/slice_sample_test_2.jl
@@ -15,7 +15,9 @@ rng = SplittableRandom(1)
 function main()
     for i in 1:100
         Pigeons.slice_sample!(h, vi, log_potential, rng)
+        DynamicPPL.invlink!!(vi, model)
         println(vi.metadata[1].vals)
+        DynamicPPL.link!(vi, DynamicPPL.SampleFromPrior())
     end
 end
 
