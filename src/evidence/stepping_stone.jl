1f873cbb826ebc5907435e6ab19d32eb8f1fb6b4
Alexandre Bouchard <alexandre.bouchard@gmail.com>
2023-01-17 15:20:58 -0800

    Rm leftover printline

--- src/evidence/stepping_stone.jl
+++ src/evidence/stepping_stone.jl
@@ -1,7 +1,6 @@
 function stepping_stone_pair(log_sum_ratios::GroupBy)
     estimator1 = 0.0
     estimator2 = 0.0
-    println("keys: $(keys(log_sum_ratios.value))")
     for (i, j) in keys(log_sum_ratios.value)
         log_sum_ratio = log_sum_ratios[(i, j)]
         current = value(log_sum_ratio) - log(log_sum_ratio.n)
