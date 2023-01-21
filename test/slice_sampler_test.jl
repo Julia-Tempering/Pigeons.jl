4e7d4f0a32b088f7912c497a3f332c6866e6e2b1
Nikola Surjanovic <16364942+nikola-sur@users.noreply.github.com>
2023-01-20 17:56:59 -0800

    Add nice test for AbstractVector

--- test/slice_sampler_test.jl
+++ test/slice_sampler_test.jl
@@ -8,25 +8,22 @@ import Pigeons: SliceSampler, slice_sample!
 Run from runtests.jl
 """
 
-function test_slice_sampler()
+function test_slice_sampler_vector()
     rng = MersenneTwister(1)
-    # log_potential = (x) -> logpdf(Normal(0.0, 1.0), x[1])
-    # log_potential = (x) -> logpdf(Normal(0.0, 1.0), x[1]) + logpdf(Normal(3.0, 1.0), x[2])
-    # log_potential = (x) -> logpdf(Bernoulli(0.5), x[1])
     log_potential = (x) -> logpdf(Bernoulli(0.5), x[1]) + logpdf(Normal(0.0, 1.0), x[2])
-    n = 1000
     h = SliceSampler()
-    states = Vector{Vector{Float64}}(undef, n)
-    state = Any[0, 0.0] # change to float or int depending on model...
+    state = Any[0, 0.0]
+    n = 1000
+    states = Vector{typeof(state)}(undef, n)
     for i in 1:n
         slice_sample!(h, state, log_potential, rng)
-        println(state)
         states[i] = copy(state)
     end
-    println(mean(states))
-    println(std(states))
-    # @assert abs(mean(states)[1] - 0.0) ≤ 0.1
-    # @assert abs(std(states)[1] - 1.0) ≤ 0.1
+    @assert all(abs.(mean(states) - [0.5, 0.0]) .≤ 0.1)
+    @assert all(abs.(std(states) - [0.5, 1.0]) .≤ 0.1)
 end
 
-test_slice_sampler()
+function test_slice_sampler()
+    test_slice_sampler_vector()
+    # test_slice_sampler_Turing()
+end
