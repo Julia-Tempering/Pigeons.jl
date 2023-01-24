8f74259d4cdde65a9848e2b42d9a99cb2e7002e9
Alexandre Bouchard <alexandre.bouchard@gmail.com>
2023-01-24 13:17:11 -0800

    Improvements to MPI utils

--- src/submission/submission_utils.jl
+++ src/submission/submission_utils.jl
@@ -15,6 +15,7 @@ function kill_job(result)
     exec_folder = result.exec_folder 
     submission_code = readline("$exec_folder/info/submission_output.txt")
     run(`qdel $submission_code`)
+    return nothing
 end
 
 function watch(result; machine = 1, last_n_lines = 20)
@@ -22,14 +23,21 @@ function watch(result; machine = 1, last_n_lines = 20)
     output_folder = "$(result.exec_folder)/1"
     output_file_name = find_rank_file(output_folder, machine)
     stdout_file = "$output_folder/$output_file_name/stdout"
-    run(`tail -n $last_n_lines $stdout_file`) # -f is nicer but crashes when 
+    run(`tail -n $last_n_lines $stdout_file`) # -f is nicer but crashes
+    return nothing 
 end
 
 function find_rank_file(folder, machine::Int)
-    for file in readdir(folder)
-        if match(Regex(".*rank.0*$machine"), file) !== nothing
-            return file
+    @assert machine > 0 "using 0-index convention"
+    machine = machine - 1 # translate to MPI's 0-index ranks
+    try
+        for file in readdir(folder)
+            if match(Regex(".*rank.0*$machine"), file) !== nothing
+                return file
+            end
         end
+    catch e 
+        @warn e 
     end
-    error("Standard out file not found")
+    error("Standard out file not found (you may want to try again once the job is started)")
 end
