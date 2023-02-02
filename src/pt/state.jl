294190e9f12fb9dca39b1ab2eb173b37cbb4fba4
Alexandre Bouchard <alexandre.bouchard@gmail.com>
2023-02-02 14:01:26 -0800

    Getting rid of informal for state for now

--- src/pt/state.jl
+++ src/pt/state.jl
@@ -1,20 +1,14 @@
+
 """ 
-The state held in each Parallel Tempering [`Replica`](@ref). 
-This interface is only needed for variation Parallel Tempering and for 
-some recorders such as [`OnlineStateRecorder`](@ref).
+$SIGNATURES 
+
+The names (each a `Symbol`) of the continuous variables in the given state. 
 """
-@informal state begin
-    """ 
-    $SIGNATURES 
+continuous_variables(state) = @abstract 
 
-    The names (each a `Symbol`) of the continuous variables in the given [`state`](@ref). 
-    """
-    continuous_variables(state) = @abstract 
+"""
+$SIGNATURES 
 
-    """
-    $SIGNATURES 
-
-    The storage within the [`state`](@ref) of the variable of the given name, typically an `Array`.
-    """
-    variable(state, name::Symbol) = @abstract 
-end
+The storage within the state of the variable of the given name, typically an `Array`.
+"""
+variable(state, name::Symbol) = @abstract 
