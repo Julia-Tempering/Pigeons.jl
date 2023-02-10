89a403471492167c3e70c5ae6df70d25f925a742
Alexandre Bouchard <alexandre.bouchard@gmail.com>
2023-02-10 12:47:17 -0800

    Small MPI admin improvements

--- src/submission/submission_utils.jl
+++ src/submission/submission_utils.jl
@@ -5,10 +5,12 @@ Display the queue status for one MPI job.
 """ 
 function queue_status(result::Result)
     exec_folder = result.exec_folder 
-    submission_code = readline("$exec_folder/info/submission_output.txt")
+    submission_code = queue_code(result)
     run(`qstat -x $submission_code`)
     return nothing
 end
+
+queue_code(result::Result) = readline("$exec_folder/info/submission_output.txt")
 
 """ 
 $SIGNATURES 
@@ -49,31 +51,37 @@ end
 """ 
 $SIGNATURES 
 
-Print the queue status as well as the `last_n_lines` standard out 
+Print the queue status as well as the standard out 
 and error streams (merged) for the given `machine`. 
 """
-function watch(result::Result; machine = 1, last_n_lines = 20)
+function watch(result::Result; machine = 1)
+    @assert machine > 0 "using 0-index convention"
     queue_status(result)
+    
     output_folder = "$(result.exec_folder)/1"
+    print("Waiting")
+    while !isfile(output_folder) || find_rank_file(output_folder, machine) === nothing
+        sleep(10)
+        print(".")
+    end
+    println()
+
     output_file_name = find_rank_file(output_folder, machine)
     stdout_file = "$output_folder/$output_file_name/stdout"
-    run(`tail -n $last_n_lines $stdout_file`) # -f is nicer but crashes
+    
+    println("Watching machine $machine stdout (note ctrl-c will not kill job, just stop monitoring):")
+    run(`tail -n $last_n_lines $stdout_file`) 
     return nothing 
 end
 
 # internal
 
 function find_rank_file(folder, machine::Int)
-    @assert machine > 0 "using 0-index convention"
     machine = machine - 1 # translate to MPI's 0-index ranks
-    try
-        for file in readdir(folder)
-            if match(Regex(".*rank.0*$machine"), file) !== nothing
-                return file
-            end
+    for file in readdir(folder)
+        if match(Regex(".*rank.0*$machine"), file) !== nothing
+            return file
         end
-    catch e 
-        @warn e 
     end
-    error("Standard out file not found (you may want to try again once the job is started)")
+    return nothing
 end
