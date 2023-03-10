f75a280352ede950ef89f753f62718db9a311032
Alexandre Bouchard <alexandre.bouchard@gmail.com>
2023-03-10 09:16:58 -0800

    toy_mvn not enough to manifest ghostbug, trying Turing

--- test/runtests.jl
+++ test/runtests.jl
@@ -79,28 +79,28 @@ end
     recorder_builders = [swap_acceptance_pr, index_process, log_sum_ratio, round_trip, energy_ac1]
 
     # test swapper 
-    pigeons(
-        target = toy_mvn_target(1), 
-        n_rounds = 10,
-        checked_round = 3, 
-        recorder_builders = recorder_builders,
-        checkpoint = true, 
-        on = ChildProcess(
-                n_local_mpi_processes = n_mpis,
-                n_threads = 2)) 
-
-    # # Turing:
     # pigeons(
-    #     target = TuringLogPotential(flip_model_unidentifiable()), 
+    #     target = toy_mvn_target(1), 
     #     n_rounds = 10,
     #     checked_round = 3, 
-    #     multithreaded = true,
     #     recorder_builders = recorder_builders,
     #     checkpoint = true, 
     #     on = ChildProcess(
-    #             dependencies = [Distributions, DynamicPPL, LinearAlgebra, "turing.jl"],
     #             n_local_mpi_processes = n_mpis,
-    #             n_threads = 2))
+    #             n_threads = 2)) 
+
+    # # Turing:
+    pigeons(
+        target = TuringLogPotential(flip_model_unidentifiable()), 
+        n_rounds = 10,
+        checked_round = 3, 
+        multithreaded = true,
+        recorder_builders = recorder_builders,
+        checkpoint = true, 
+        on = ChildProcess(
+                dependencies = [Distributions, DynamicPPL, LinearAlgebra, "turing.jl"],
+                n_local_mpi_processes = n_mpis,
+                n_threads = 2))
     # # Blang:
     # if !Sys.iswindows() # JNI crashes on windows; see commit right after c016f59c84645346692f720854b7531743c728bf
     #     Pigeons.setup_blang("blangDemos")
