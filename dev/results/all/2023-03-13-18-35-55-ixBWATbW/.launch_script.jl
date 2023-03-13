d177f819cb89085355bc12fd74d3d2af98d5937b
Documenter.jl <documenter@juliadocs.github.io>
2023-03-13 18:40:01 +0000

    build based on 4eb663c

---
+++ dev/results/all/2023-03-13-18-35-55-ixBWATbW/.launch_script.jl
@@ -0,0 +1,8 @@
+using Serialization
+using Pigeons
+
+
+Pigeons.deserialize_immutables(raw"/home/runner/work/Pigeons.jl/Pigeons.jl/docs/build/results/all/2023-03-13-18-35-55-ixBWATbW/immutables.jls")
+pt_arguments = deserialize(raw"/home/runner/work/Pigeons.jl/Pigeons.jl/docs/build/results/all/2023-03-13-18-35-55-ixBWATbW/.pt_argument.jls")
+pt = PT(pt_arguments, exec_folder = raw"/home/runner/work/Pigeons.jl/Pigeons.jl/docs/build/results/all/2023-03-13-18-35-55-ixBWATbW")
+pigeons(pt)
