fa848c36792bae34f2cf57cd56e1c9477ca8f1db
Nikola Surjanovic <16364942+nikola-sur@users.noreply.github.com>
2023-03-31 16:51:34 -0700

    Try to fix docs part 4

--- src/var_reference/var_reference.jl
+++ src/var_reference/var_reference.jl
@@ -30,13 +30,13 @@ A variational family of reference distributions.
     $SIGNATURES
     Obtain one iid sample from the reference distribution specified by the variational family.
     """
-    sample_iid!(var_reference, replica) = @abstract
+    sample_iid!(var_reference::VarReference, replica) = @abstract
 
     """
     $SIGNATURES
     Evaluate the log density of the variational reference at a point `x`.
     """
-    (var_reference)(state) = @abstract
+    (var_reference::VarReference)(state) = @abstract
 end
 
 
