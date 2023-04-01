729c2544a614d7510e172fa273da475529999054
Alexandre Bouchard <alexandre.bouchard@gmail.com>
2023-04-01 09:40:03 -0700

    Progressing on fused swaps

--- src/swap/FusedSwap.jl
+++ src/swap/FusedSwap.jl
@@ -12,66 +12,90 @@ const fused_swap_tol = Ref(1e-5)
 
 function swap_stat(pair_swapper::FusedSwap, replica::Replica, partner_chain::Int) 
     # everythig will be in place, so save current location along the orbit
-    current = current_position(pair_swapper, replica)
+    current_t, mover = state_mover(pair_swapper, replica)
 
-    # compute "pre-involution" i.e. moved point s.t. we have not checked yet that the involutive prop holds
-    fwd_obj_and_deriv = objective_and_derivative(pair_swapper, replica, partner_chain, replica.chain)
-    bwd_obj_and_deriv = objective_and_derivative(pair_swapper, replica, replica.chain, partner_chain)
+    T, dT = height_mover(pair_swapper, replica.chain, partner_chain)
+    W_mine,  dW_mine  = log_density_slice(mover, pair_swapper.log_potentials[replica.chain])
+    W_yours, dW_yours = log_density_slice(mover, pair_swapper.log_potentials[partner_chain]) 
 
-    proposed = pre_involution(fwd_obj_and_deriv, current) # proposed is NaN if solution not found
-    reversed = pre_involution(bwd_obj_and_deriv, proposed)
+    current_height  = W_mine(current_state)
+    proposed_height = T(current_height)
 
-    checked = isapprox(current, reversed; atol = fused_swap_tol[]) # if NaN, 'checked' and hence 'fused' will be false
-    fused = checked && proposed != current # are use doing a 'fused move' (where both x and beta change)? otherwise, classical swap where only beta's are exchanged
+    # compute "pre-involution" i.e. proposed state move s.t. we have not checked yet that the involutive property holds
+    proposed_t     = pre_involution(W_yours, dW_yours, current_t,  proposed_height) 
+    
+    if isnan(proposed_t) # i.e. root finding in pre_involution failed
+        fused = false
+    else
+        reversed_t = pre_involution(W_mine,  dW_mine,  proposed_t, current_height)
+        checked = isapprox(current_t, reversed_t; atol = fused_swap_tol[]) # if reversed_t is NaN, 'checked' and hence 'fused' will be false
+        fused = checked && proposed_t != current_t # are use doing a 'fused move' (where both x and beta change)? otherwise, classical swap where only beta's are exchanged
+    end
 
     if !fused # then use classical ratio:
         log_ratio = log_unnormalized_ratio(log_potentials, partner_chain, replica.chain, replica.state)
-        move!(pair_swapper, replica, current)
-        return FusedStat(log_ratio, rand(replica.rng), current)
+        move!(mover, current_t)
+        return FusedStat(log_ratio, rand(replica.rng), current_t)
     end
 
-    W_my_prime = bwd_obj_and_deriv[2]
-    W_partner_prime = fwd_obj_and_deriv[2] 
-
-    log_ratio = log(W_my_prime(current)) - log(W_partner_prime(proposed)) - log(transport_deriv(pair_swapper, current))
+    log_ratio = logabs(dW_mine(current_t)) - logabs(dW_yours(proposed_t)) + logabs(dT(current_height))
     
     # go back to current point, will do the actual moving after the accept-reject step
-    move!(pair_swapper, replica, current)
-    return FusedStat(log_ratio, rand(replica.rng), proposed)
+    move!(mover, current_t)
+    return FusedStat(log_ratio, rand(replica.rng), proposed_t)
 end
 
-function transport_deriv(pair_swapper, current)
-    function fct(point)
-        
-    end
-end
+logabs(x) = log(abs(x))
 
-function objective_and_derivative(pair_swapper::FusedSwap, replica::Replica, partner_chain, my_chain)
-    current_height = pair_swapper.log_potentials[my_chain](replica.state) 
-    transported_height = transport_height(pair_swapper, partner_chain, my_chain, current_height)
-    function objective(point)
-        move!(pair_swapper, replica, point)
-        return pair_swapper.log_potentials[partner_chain](replica.state) - transported_height
-    end
-    deriv(point) = ForwardDiff.derivative(objective, point)
-    return (objective, deriv) 
-end
-
-function pre_involution(obj_and_deriv, start_point)
-    problem = ZeroProblem(obj_and_deriv, start_point)
+function pre_involution(W, dW, start_point, proposed_height)
+    shifted_W(x) = W(x) - proposed_height
+    problem = ZeroProblem((shifted_W, dW), start_point)
     return solve(problem, atol = fused_swap_tol[] / 2.0)  
 end
 
-function move!(pair_swapper::FusedSwap, replica::Replica, to)
-    # move by delta in place along the flow
+@concrete mutable struct StateMover
+    moved_t
+    replica
 end
 
-function current_position(pair_swapper::FusedSwap, replica::Replica)
-
+function state_mover(pair_swapper::FusedSwap, replica)
+    # consider set of point {exp(t) x : t in Real}
+    current_t = 0.0
+    mover = StateMover(current_t, replica)
+    return current_t, mover
 end
 
+function move!(mover::StateMover, to)
+    if to == mover.moved_t
+        return 
+    end
+    # current replica state is cur = exp(t) x
+    # you want to got to       new = exp(t') x
+    # we have: new = exp(t') x = (exp(t) / exp(t)) exp(t') x = (exp(t') / exp(t)) ( exp(t) x ) = (exp(t' - t)) cur
+    multiplier = exp(to - mover.current_t)
+    scale!(mover.replica, multiplier)
+end
 
+function log_density_slice(mover, log_potential)
+    function W(t)
+        move!(mover, t) 
+        return log_potential(mover.replica.state)
+    end 
+    function dW(t)
+        move!(mover, t)
+        # since the derivative of exp(t) is exp(t) this is just the directional derivative at x_t and along x_t
+        return directional_derivative(log_potential, mover.replica.state, mover.replica.state) 
+    end
+    return W, dW
+end
 
+function directional_derivative(log_potential, x, v)
+    # NB: may want to use FwdDiff with a given direction
+    # seems implemented in https://github.com/JuliaDiff/SparseDiffTools.jl/blob/master/src/differentiation/jaches_products.jl#L3-L13
+    # but does not have public API at the moment??? https://github.com/JuliaDiff/ForwardDiff.jl/issues/319
+    gradient = gradient(log_potential, x)
+    return dot(gradient, v)
+end
 
 function record_swap_stats!(pair_swapper::FusedSwap, recorders, chain1::Int, stat1, chain2::Int, stat2)
     acceptance_pr = swap_acceptance_probability(stat1, stat2)
