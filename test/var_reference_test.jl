62d70c33ee3303d1bb5e662824d5a5ab1bf55cb3
Nikola Surjanovic <16364942+nikola-sur@users.noreply.github.com>
2023-04-07 13:23:54 -0700

    Delete VectorLogPotential

--- test/var_reference_test.jl
+++ test/var_reference_test.jl
@@ -7,49 +7,20 @@ function test_var_reference_Turing()
     
     # Check NoVarReference()
     inputs = Inputs(
-    target = TuringLogPotential(model),
-    n_chains = 10,
-    n_chains_var_reference = 0,
-    seed = 1
+        target = TuringLogPotential(model),
+        n_chains = 10,
+        n_chains_var_reference = 0,
+        seed = 1
     )
     @test_nowarn pt = pigeons(inputs)
     
     # Check GaussianReference()
     inputs = Inputs(
-    target = TuringLogPotential(model),
-    n_chains = 0,
-    n_chains_var_reference = 10,
-    var_reference = GaussianReference(),
-    seed = 1
-    )
-    @test_nowarn pt = pigeons(inputs)
-end
-
-
-function test_var_reference_vector()
-    target = VectorLogPotential(
-        Normal_2D,
-        Normal_2D_reference,
-        Normal_2D_reference_sample!,
-        2
-    )
-    
-    # Check NoVarReference()
-    inputs = Inputs(
-    target                  = target,
-    n_chains                = 10,
-    n_chains_var_reference  = 0,
-    seed                    = 1
-    )
-    @test_nowarn pt = pigeons(inputs)
-    
-    # Check GaussianReference()
-    inputs = Inputs(
-    target                  = target,
-    n_chains                = 0,
-    n_chains_var_reference  = 10,
-    seed                    = 1,
-    var_reference            = GaussianReference()
+        target = TuringLogPotential(model),
+        n_chains = 0,
+        n_chains_var_reference = 10,
+        var_reference = GaussianReference(),
+        seed = 1
     )
     @test_nowarn pt = pigeons(inputs)
 end
@@ -57,5 +28,4 @@ end
 
 function test_var_reference()
     test_var_reference_Turing()
-    test_var_reference_vector()
 end
