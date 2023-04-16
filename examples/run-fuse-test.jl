61f219da818395fbfa2c2f95a03960c6c4d677ff
Alexandre Bouchard <alexandre.bouchard@gmail.com>
2023-04-15 20:34:18 -0700

    Add switch to easily run exp and normal models

--- examples/run-fuse-test.jl
+++ examples/run-fuse-test.jl
@@ -4,26 +4,38 @@ Pkg.activate(".")
 using Revise
 using Pigeons 
 using Statistics
+using Distributions
 
 target_rate = 2.0
 
+@show use_exp = false
+
 pt = pigeons(
-        target = Pigeons.ExpDist(target_rate), #Product(Normal.(zeros(gauss_dim), 2 * ones(gauss_dim))), 
+        target = 
+            use_exp ?
+                Pigeons.ExpDist(target_rate) :
+                Product(Normal.(zeros(Pigeons.gauss_dim), 2 * ones(Pigeons.gauss_dim))), 
         n_rounds = 10,
-        n_chains = 2,
+        n_chains = 10,
         fused_swaps = false,
         recorder_builders = [Pigeons.online_recorder_builders(); Pigeons.interpolated_log_potentials]
     )
 
 pt = pigeons(
-        target = Pigeons.ExpDist(target_rate), #Product(Normal.(zeros(gauss_dim), 2 * ones(gauss_dim))), 
-        n_rounds = 20,
+        target = 
+            use_exp ?
+                Pigeons.ExpDist(target_rate) :
+                Product(Normal.(zeros(Pigeons.gauss_dim), 2 * ones(Pigeons.gauss_dim))), 
+        n_rounds = 15,
         n_chains = 2,
         fused_swaps = true,
         recorder_builders = [Pigeons.online_recorder_builders(); Pigeons.interpolated_log_potentials]
     )
 
-@show mean(pt), var(pt)
+
+
+
+#@show mean(pt), var(pt)
 
 beta = 1.0
 points, cumulative = Pigeons.interpolated_log_potential_distribution(pt, beta, 0)
@@ -47,7 +59,7 @@ analytic_iF(x) = log(analytic_rate * x)
 using Plots
 
 range = 0.0001:0.0001:0.9999 
-range2 = (f-5):0.1:(l+5)
+range2 = -10:0.1:10 # (f-5):0.1:(l+5)
 
 # p1 = plot(fct, range2)
 # plot!(analytic_F, range2)
@@ -63,12 +75,12 @@ range2 = (f-5):0.1:(l+5)
 
 
 Te, dTe = Pigeons.height_mover(pt.shared.swapper, 1, 2)
-Ta, dTa, a, b = Pigeons.detailed_a_height_mover(pt.shared.swapper, 1, 2)
+#Ta, dTa, a, b = Pigeons.detailed_a_height_mover(pt.shared.swapper, 1, 2)
 
 p1 = plot(Te, range2)
-plot!(Ta, range2)
+#plot!(Ta, range2)
 
 p2 = plot(dTe, range2)
-plot!(dTa, range2)
+#plot!(dTa, range2)
 
 plot(p1, p2)
