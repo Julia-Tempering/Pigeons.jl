437e1797608f55c18a2c9e6e2d1872a216305c9b
Documenter.jl <documenter@juliadocs.github.io>
2023-04-16 04:20:40 +0000

    build based on 820c1c5

--- dev/results/all/2023-04-12-03-32-18-T56CGCkP/.launch_script.jl
+++
@@ -1,8 +0,0 @@
-using Serialization
-using Pigeons
-Pigeons.mpi_active_ref[] = true
-
-Pigeons.deserialize_immutables(raw"/home/runner/work/Pigeons.jl/Pigeons.jl/docs/build/results/all/2023-04-12-03-32-18-T56CGCkP/immutables.jls")
-pt_arguments = deserialize(raw"/home/runner/work/Pigeons.jl/Pigeons.jl/docs/build/results/all/2023-04-12-03-32-18-T56CGCkP/.pt_argument.jls")
-pt = PT(pt_arguments, exec_folder = raw"/home/runner/work/Pigeons.jl/Pigeons.jl/docs/build/results/all/2023-04-12-03-32-18-T56CGCkP")
-pigeons(pt)
---
+++ dev/results/all/2023-04-16-04-15-36-0NEi4kfx/.launch_script.jl
@@ -0,0 +1,8 @@
+using Serialization
+using Pigeons
+
+
+Pigeons.deserialize_immutables(raw"/home/runner/work/Pigeons.jl/Pigeons.jl/docs/build/results/all/2023-04-16-04-15-36-0NEi4kfx/immutables.jls")
+pt_arguments = deserialize(raw"/home/runner/work/Pigeons.jl/Pigeons.jl/docs/build/results/all/2023-04-16-04-15-36-0NEi4kfx/.pt_argument.jls")
+pt = PT(pt_arguments, exec_folder = raw"/home/runner/work/Pigeons.jl/Pigeons.jl/docs/build/results/all/2023-04-16-04-15-36-0NEi4kfx")
+pigeons(pt)
