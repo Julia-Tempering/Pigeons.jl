04a5c8e26da1b8326ba6ab88d35c126e87e55c0f
Alexandre Bouchard <alexandre.bouchard@gmail.com>
2023-04-21 15:39:36 -0700

    Fix

--- examples/black-hole-imaging-mpi.jl
+++ examples/black-hole-imaging-mpi.jl
@@ -1,11 +1,11 @@
 include("comrade-interface.jl")
 
 r = pigeons(
-        target = comrade_target, 
-        n_rounds = 5, 
-        n_chains = 10, 
+        target = comrade_target_example(), 
+        n_rounds = 10, 
+        n_chains = 100, 
         on = MPI(
-            n_mpi_processes = 2, 
+            n_mpi_processes = 100, 
             dependencies = ["comrade-interface.jl"]))
 
 # use watch(r) to see the output
