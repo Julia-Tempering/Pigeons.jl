32e20ac0401e43c79d7b627a736555b8be5b3e6f
nikola-sur <nikola.surjanovic@stat.ubc.ca>
2023-04-26 17:18:59 -0700

    Restore old file and add test

--- test/runtests.jl
+++ test/runtests.jl
@@ -121,6 +121,7 @@ end
     # make sure the examples run correctly
     include("../examples/custom-path.jl")
     include("../examples/general-target.jl")
+    include("../examples/general-reference.jl")
 end
 
 @testset "Check sources can be sorted automatically" begin
