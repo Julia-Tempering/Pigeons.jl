9674066ba59b52f07423fdd6ba93653cb64eb088
Alexandre Bouchard <alexandre.bouchard@gmail.com>
2023-04-27 22:45:07 -0700

    Fix last commit which broke deserialization, trying different method

--- src/targets/LazyTarget.jl
+++ src/targets/LazyTarget.jl
@@ -5,41 +5,45 @@ be serialized, e.g. FFT plans
 so that the target is constructed just in time by each MPI node. 
 
 ```
+# in a script.jl:
 struct MyTargetFlag end 
 import Pigeons.instantiate_target
 Pigeons.instantiate_target(flag::MyTargetFlag) = toy_mvn_target(1)
-pigeons(target = Pigeons.LazyTarget(MyTargetFlag())
+
+# to run
+pigeons(target = Pigeons.LazyTarget(MyTargetFlag(), on = ChildProcess(dependencies = ["script.jl"]))
 ```
+
+Note: should only be used in the context of ChildProcess() / MPI() 
+as it assumes that a single call to pigeons() will be made in the 
+lifetime of the process. 
 """
-mutable struct LazyTarget # {FlagType, ActualType} - NO: omitting types on purpose: they could leak non-serializability
-    flag
-    instance
-    LazyTarget(flag) = new(flag, nothing)
+struct LazyTarget{FlagType}
+    flag::FlagType
 end
+
+# Note: we keep that in a global rather than in the LazyTarget 
+# b/c we dont want type info of the product to leak into the 
+# serialization; leading it untyped fails too at serialization time
+const _lazy_singleton_cache = Dict{Any, Any}()
 
 instantiate_target(flag) = @abstract 
 
-ensure_instantiated(lazy) =
-    if lazy.instance === nothing 
-        lazy.instance = instantiate_target(lazy.flag)
+function get_lazy_singleton(lazy) 
+    if !haskey(_lazy_singleton_cache, lazy.flag)
+        _lazy_singleton_cache[lazy.flag] = instantiate_target(lazy.flag)
     end
-
-function create_state_initializer(lazy::LazyTarget, inputs::Inputs) 
-    ensure_instantiated(lazy)
-    create_state_initializer(lazy.instance, inputs)
+    return _lazy_singleton_cache[lazy.flag]
 end
 
-function default_explorer(lazy::LazyTarget) 
-    ensure_instantiated(lazy)
-    default_explorer(lazy.instance)
-end
+create_state_initializer(lazy::LazyTarget, inputs::Inputs) =
+    create_state_initializer(get_lazy_singleton(lazy), inputs)
 
-function create_reference_log_potential(lazy::LazyTarget, inputs::Inputs) 
-    ensure_instantiated(lazy)
-    create_reference_log_potential(lazy.instance, inputs)
-end
+default_explorer(lazy::LazyTarget) =
+    default_explorer(get_lazy_singleton(lazy))
 
-function create_path(lazy::LazyTarget, inputs::Inputs) 
-    ensure_instantiated(lazy)
-    create_path(lazy.instance, inputs)
-end
+create_reference_log_potential(lazy::LazyTarget, inputs::Inputs) =
+    create_reference_log_potential(get_lazy_singleton(lazy), inputs)
+
+create_path(lazy::LazyTarget, inputs::Inputs) =
+    create_path(get_lazy_singleton(lazy), inputs)
