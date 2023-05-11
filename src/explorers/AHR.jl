77eb7974ec628d26024b6d332bf25945b2d23b2a
Alexandre Bouchard <alexandre.bouchard@gmail.com>
2023-05-11 16:18:52 -0700

    Non-allocating AHR

--- src/explorers/AHR.jl
+++ src/explorers/AHR.jl
@@ -5,33 +5,44 @@
     n_passes::Int = 3 # n_passes through all variables per exploration step
 end
 
-@auto mutable struct Ray 
+@auto struct Ray 
     start
     state
     direction 
-    current
 end
 
-Base.getindex(ptr::Ray) = ptr.current 
 function Base.setindex!(ptr::Ray, value)
-    ptr.current = value
     ptr.state .= ptr.start .+ ptr.direction .* value
     return nothing
 end
 
+Base.getindex(ptr::Ray) =
+    (ptr.state[1] - ptr.start[1]) / ptr.direction[1]
 
 function step!(explorer::AHR, replica, shared) 
+
     slicer = SliceSampler(explorer.w, explorer.p, 1)
     log_potential = find_log_potential(replica, shared)
     rng = replica.rng
     state = replica.state
+
+    dim = length(state)
+    state_buffer = get_buffer(replica.recorders.ahr_state_buffer, dim)
+    direction_buffer = get_buffer(replica.recorders.ahr_direction_buffer, dim)
+
     for i in 1:explorer.n_passes 
-        start = copy(state) 
-        direction = randn(rng, length(start))
-        direction ./= norm(direction) 
-        pointer = Ray(start, state, direction, 0.0)
+        state_buffer .= state
+        randn!(rng, direction_buffer)
+        direction_buffer ./= norm(direction_buffer) 
+        pointer = Ray(state_buffer, state, direction_buffer)
         z = log_potential(state) - rand(rng, Exponential(1.0))
         L, R = slice_double(slicer, state, z, pointer, log_potential, rng)
         pointer[] = slice_shrink(slicer, state, z, L, R, pointer, log_potential, rng)
     end
 end
+
+explorer_recorder_builders(hmc::AHR) = 
+    [ahr_state_buffer, ahr_direction_buffer]
+
+ahr_state_buffer() = Augmentation{Vector{Float64}}()
+ahr_direction_buffer() = Augmentation{Vector{Float64}}()
