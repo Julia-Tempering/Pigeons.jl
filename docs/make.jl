2a1608e0d1b92bae7568c9bcacddbea06a0c7231
Alexandre Bouchard <alexandre.bouchard@gmail.com>
2023-05-29 12:30:47 -0700

    Can we have a more normal Project.toml for testing?

--- docs/make.jl
+++ docs/make.jl
@@ -1,9 +1,8 @@
-using Pkg
-
 # make sure we are using the version contained 
 # in whatever state the parent directory is; 
 # this is the intended behaviour both for CI and 
 # local development
+using Pkg
 script_dir = @__DIR__
 parent_dir = dirname(script_dir)
 Pkg.develop(PackageSpec(path=parent_dir))
--- test/runtests.jl
+++ test/runtests.jl
@@ -1,3 +1,14 @@
+# make sure we are using the version contained 
+# in whatever state the parent directory is; 
+# this is the intended behaviour both for CI and 
+# local development
+using Pkg
+script_dir = @__DIR__
+parent_dir = dirname(script_dir)
+Pkg.develop(PackageSpec(path=parent_dir))
+
+
+
 using Pigeons
 
 using ArgMacros
