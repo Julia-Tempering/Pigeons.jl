cedbd8d3c9d76158d1ed463b1825a96ea3820945
Alexandre Bouchard <alexandre.bouchard@gmail.com>
2023-05-29 21:56:42 -0700

    Polish

--- test/runtests.jl
+++ test/runtests.jl
@@ -1,6 +1,6 @@
 include("supporting/setup.jl")
 
-# check we are testing the checkout version of the repo, not e.g. latest released version
+# check we are testing the checked-out version of the repo, not e.g. latest released version
 test_dir = @__DIR__ 
 @assert basename(test_dir) == "test"
 project_root_dir = dirname(test_dir)
@@ -8,6 +8,8 @@ project_root_dir = dirname(test_dir)
 
 # load all files starting with "test_"
 for test_name in filter(x -> startswith(x, "test_") && endswith(x, ".jl"), readdir(test_dir)) 
+    # organize output a little bit
+    println() # v otherwise can't tell what is running when it crashes in the middle
     println("### Starting $test_name")
              # + - yes, we need this horror, because we are dealing with a macro
              # v 
