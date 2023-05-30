244406cb7f287f36744993379ccc83fbd75b9f5b
Alexandre Bouchard <alexandre.bouchard@gmail.com>
2023-05-29 17:09:43 -0700

    Start improving coverage

--- test/test_parallelism_invariance.jl
+++ test/test_parallelism_invariance.jl
@@ -8,7 +8,7 @@ include("supporting/mpi_test_utils.jl")
     recorder_builders = [swap_acceptance_pr, index_process, log_sum_ratio, round_trip, energy_ac1]
 
     # two explorers 
-    for explorer in [SliceSampler(), AutoMALA()]
+    for explorer in [SliceSampler(), AutoMALA(), Compose(SliceSampler(), AutoMALA())]
         pigeons(;
             target = toy_mvn_target(1), 
             n_rounds = 10,
@@ -23,18 +23,20 @@ include("supporting/mpi_test_utils.jl")
     end
 
     # Turing:
-    pigeons(
-        target = TuringLogPotential(flip_model_unidentifiable()), 
-        n_rounds = 10,
-        checked_round = 3, 
-        multithreaded = true,
-        recorder_builders = recorder_builders,
-        checkpoint = true, 
-        on = ChildProcess(
-                dependencies = [Distributions, DynamicPPL, LinearAlgebra, joinpath(@__DIR__, "supporting/turing_models.jl")],
-                n_local_mpi_processes = n_mpis,
-                n_threads = 2,
-                mpiexec_args = extra_mpi_args()))
+    for model in [flip_model_unidentifiable(), flip_mixture()]
+        pigeons(
+            target = TuringLogPotential(flip_model_unidentifiable()), 
+            n_rounds = 4,
+            checked_round = 3, 
+            multithreaded = true,
+            recorder_builders = recorder_builders,
+            checkpoint = true, 
+            on = ChildProcess(
+                    dependencies = [Distributions, DynamicPPL, LinearAlgebra, joinpath(@__DIR__, "supporting/turing_models.jl")],
+                    n_local_mpi_processes = n_mpis,
+                    n_threads = 2,
+                    mpiexec_args = extra_mpi_args()))
+    end
 
     # Blang:
     if !Sys.iswindows() # JNI crashes on windows; see commit right after c016f59c84645346692f720854b7531743c728bf
