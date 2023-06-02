166e81c6e69d5b9c1fc7a14ce0fe65d8a600f2fa
nikola-sur <nikola.surjanovic@stat.ubc.ca>
2023-06-02 15:25:17 -0700

    one more example

--- test/nikola_temp/stan_example.jl
+++ test/nikola_temp/stan_example.jl
@@ -1,21 +1,19 @@
-using Pigeons
+using Pigeons 
 using BridgeStan
 const BS = BridgeStan
-using Random 
-using SplittableRandoms
 using Plots
 
 function main()
     # settings
-    bernoulli_stan = "test/nikola_temp/bernoulli.stan"
-    bernoulli_data = "test/nikola_temp/bernoulli.data.json"
+    stan = "test/nikola_temp/eight_schools_noncentered.stan"
+    data = "test/nikola_temp/eight_schools.json"
 
     # PT settings
-    n_rounds = 10
+    n_rounds = 3
     n_chains = 10
 
     # create Stan models
-    smb = BS.StanModel(stan_file = bernoulli_stan, data = bernoulli_data)
+    smb = BS.StanModel(stan_file = stan, data = data)
     slp = StanLogPotential(smb)
 
     # run Pigeons
@@ -24,10 +22,9 @@ function main()
         recorder_builders = [traces], n_chains_var_reference = n_chains, 
         var_reference = GaussianReference())
     s = get_sample(pt, n_chains)
-    samples_vec = map((x) -> x[1], s)
+    samples_vec = map((x) -> x[1], s) # get first element
     p = Plots.histogram(samples_vec, bins = -3:0.1:3)
     display(p)
-    nothing
 end
 
 main()
