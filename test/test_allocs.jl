53c4553513551d7300b17c5bb389f539a28788bd
Alexandre Bouchard <alexandre.bouchard@gmail.com>
2023-07-11 13:55:35 -0700

    Test that allocs with stan + automala are dim-independent

--- test/test_allocs.jl
+++ test/test_allocs.jl
@@ -1,3 +1,18 @@
+@testset "Allocs-Stan" begin
+    #=
+    Despite best effort, just can't get automala + stan bridge 
+    all the way to zero allocs in the inner loop. See e.g. 
+    9d85645b3422260043a59b89d049f54d782f76bc
+    However what allocs are left are now dimension-independent. 
+    This checks that a 100-fold increase in dim only increases allocs 
+    by a small factor. 
+    =#
+    allocs_1d = Pigeons.last_round_max_allocation(pigeons(n_chains = 1, n_rounds = 5, target = Pigeons.toy_stan_target(1), explorer = AutoMALA(exponent_n_refresh = 0.0)))
+    allocs_100d = Pigeons.last_round_max_allocation(pigeons(n_chains = 1, n_rounds = 5, target = Pigeons.toy_stan_target(100), explorer = AutoMALA(exponent_n_refresh = 0.0)))
+
+    @test abs(allocs_1d - allocs_100d)/allocs_1d < 3
+end
+
 @testset "Allocs-SliceSampler" begin
     allocs_10_rounds = Pigeons.last_round_max_allocation(pigeons(n_rounds = 11, target = toy_mvn_target(100)))
     allocs_11_rounds = Pigeons.last_round_max_allocation(pigeons(n_rounds = 12, target = toy_mvn_target(100)))
