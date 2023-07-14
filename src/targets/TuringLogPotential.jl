4540ae25277ef042a689f41e8c4427c61d4e9fbc
Alexandre Bouchard <alexandre.bouchard@gmail.com>
2023-07-14 13:02:55 -0700

    Document Turing autodiff limitations

--- src/targets/TuringLogPotential.jl
+++ src/targets/TuringLogPotential.jl
@@ -50,6 +50,28 @@ end
 
 LogDensityProblemsAD.dimension(log_potential::TuringLogPotential) = length(DynamicPPL.getall(initialization(log_potential)))
 function LogDensityProblemsAD.ADgradient(kind::Symbol, log_potential::TuringLogPotential, buffers::Augmentation)
+    @info   """
+            We recommend using SliceSampler() for Turing models. If you have a large continuous model 
+            consider using the StanBridge. The Turing interface is still useful for models containing 
+            both continuous and discrete variables. 
+            
+            Details:
+
+            - The LogDensityProblems interface seems to force us to keep two representations of the states, 
+              one for the VariableInfo and one vector based. This is only partly implemented at the moment, 
+              as a result we have the following limitations: (1) AutoMALA+Turing cannot use the 
+              diagonal pre-conditioning at the moment. (2) AutoMALA+Turing only works if all variables are 
+              continuous at the moment. Both could be addressed but since the autodiff is pretty slow at the
+              moment it seems low priority; the user can just rely on SliceSampler() at the moment. 
+            - If the user has a fully continuous model, there is a good alternative: the Stan Bridge, 
+              (which has much faster autodiff than Zygote, and Enzyme crashes on Turing at the time of writing). 
+            - On some Turing models, gradient computation is non-deterministic, 
+              see 4433584a044510bf9360e1e7191e59478496dc0b and associated CIs at 
+              https://github.com/Julia-Tempering/Pigeons.jl/actions/runs/5550424683/jobs/10135522013
+              vs 
+              https://github.com/Julia-Tempering/Pigeons.jl/actions/runs/5550424683/jobs/10135521940
+              (look for output of test_turing.jl)
+            """ maxlog=1
     context = log_potential.only_prior ? DynamicPPL.PriorContext() : DynamicPPL.DefaultContext()
     fct = DynamicPPL.LogDensityFunction(initialization(log_potential), log_potential.model, context)
     return ADgradient(kind, fct)
--- test/test_turing.jl
+++ test/test_turing.jl
@@ -1,10 +1,14 @@
-@testset "Turing-gradient" begin
-    # target = Pigeons.toy_turing_unid_target()
+#= 
+The reason this test is excluded is described in 
+ADgradient() in TuringLogPotential.jl
+=#
+# @testset "Turing-gradient" begin
+#     target = Pigeons.toy_turing_unid_target()
 
-    # @show Threads.nthreads()
+#     @show Threads.nthreads()
 
-    # logz_mala = stepping_stone_pair(pigeons(; target, explorer = AutoMALA(adapt_pre_conditioning = false)))
-    # logz_slicer = stepping_stone_pair(pigeons(; target, explorer = SliceSampler()))
+#     logz_mala = stepping_stone_pair(pigeons(; target, explorer = AutoMALA(adapt_pre_conditioning = false)))
+#     logz_slicer = stepping_stone_pair(pigeons(; target, explorer = SliceSampler()))
 
-    # @test abs(logz_mala[1] - logz_slicer[1]) < 0.1
-end
+#     @test abs(logz_mala[1] - logz_slicer[1]) < 0.1
+# end
