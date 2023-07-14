961960d26708939ba64e93ac5eec6a21f2bad03e
Alexandre Bouchard <alexandre.bouchard@gmail.com>
2023-07-14 08:14:07 -0700

    Get more info on last failures

--- test/test_turing.jl
+++ test/test_turing.jl
@@ -1,6 +1,8 @@
 @testset "Turing-gradient" begin
     target = Pigeons.toy_turing_unid_target()
 
+    @show Threads.nthreads()
+
     logz_mala = stepping_stone_pair(pigeons(; target, explorer = AutoMALA(adapt_pre_conditioning = false)))
     logz_slicer = stepping_stone_pair(pigeons(; target, explorer = SliceSampler()))
 
