37494563fadc1bfb8eb3607832c343b4f198fc13
Alexandre Bouchard <alexandre.bouchard@gmail.com>
2023-07-14 10:58:33 -0700

    Trying with Zygote to investimate Turing non-reproducibility

--- test/test_turing.jl
+++ test/test_turing.jl
@@ -1,9 +1,11 @@
+using Zygote
+
 @testset "Turing-gradient" begin
     target = Pigeons.toy_turing_unid_target()
 
     @show Threads.nthreads()
 
-    logz_mala = stepping_stone_pair(pigeons(; target, explorer = AutoMALA(adapt_pre_conditioning = false)))
+    logz_mala = stepping_stone_pair(pigeons(; target, explorer = AutoMALA(adapt_pre_conditioning = false, default_autodiff_backend = :Zygote)))
     logz_slicer = stepping_stone_pair(pigeons(; target, explorer = SliceSampler()))
 
     @test abs(logz_mala[1] - logz_slicer[1]) < 0.1
