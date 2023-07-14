ed048855da0e21c601c4f060651812eb5972c5e2
Alexandre Bouchard <alexandre.bouchard@gmail.com>
2023-07-14 13:15:58 -0700

    Workaround CI issue

--- test/test_blang_bridge.jl
+++ test/test_blang_bridge.jl
@@ -1,7 +1,36 @@
-@testset "Checkpoints" begin
-    # 14/7/23 - found and fixed bug (upstream in Blang SDK)
-    Pigeons.setup_blang("blangDemos")
-    Pigeons.setup_blang("nowellpack")
+
+function remove_repo(repo_name)
+    auto_install_folder = mkpath(Pigeons.mpi_settings_folder())
+    repo_path = "$auto_install_folder/$repo_name"
+    rm(repo_path, force = true, recursive=true)
+end
+
+@testset "Setup blang" begin
+
+    # 14/7/23 - found and fixed a bug (upstream in Blang SDK)
+    
+    # The gradle task sometimes fails in CI 
+    # when it tries to access the network, e.g. 
+    #   https://github.com/Julia-Tempering/Pigeons.jl/actions/runs/5557665969/jobs/10151752331
+    #   [look for java.net.SocketException: Broken pipe (Write failed)]
+    # probably some throttling related issue?
+    # As a work-around, retry up to 10 times 
+    # (when it is already installed, setup_blang() does nothing)
+    success = false
+    for i in 1:10 
+        try
+            Pigeons.setup_blang("blangDemos")
+            Pigeons.setup_blang("nowellpack")
+            success = true
+        catch e 
+            @error "Something went wrong" exception=(e, catch_backtrace())
+            remove_repo("blangDemos")
+            remove_repo("nowellpack")
+        end
+    end
+    if !success 
+        error("Unable to setup blang")
+    end
     for target in [
             Pigeons.blang_ising(), 
             Pigeons.blang_unid(), 
