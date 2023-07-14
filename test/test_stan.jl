5eeb491a33a8df875cfb15dd5c4c8788cee3a457
Alexandre Bouchard <alexandre.bouchard@gmail.com>
2023-07-14 15:43:08 -0700

    Fix Stan test

--- test/test_stan.jl
+++ test/test_stan.jl
@@ -1,6 +1,6 @@
 @testset "Stan examples" begin
-    pigeons(target = stan_eight_schools(true), n_rounds = 2, n_chains = 2)
-    pigeons(target = stan_eight_schools(false), n_rounds = 2, n_chains = 2)
+    pigeons(target = Pigeons.stan_eight_schools(true), n_rounds = 2, n_chains = 2)
+    pigeons(target = Pigeons.stan_eight_schools(false), n_rounds = 2, n_chains = 2)
 
     # some examples where an error is interpreted as -Inf:
     pigeons(target = Pigeons.stan_funnel(1), recorder_builders = [online], n_chains = 1, n_rounds = 5, explorer = SliceSampler())
