99ce6428c4c1098ff44547b4d2144155627f0595
Alexandre Bouchard <alexandre.bouchard@gmail.com>
2023-07-14 12:31:37 -0700

    Revert "Trying with Zygote to investimate Turing non-reproducibility"
    This reverts commit 37494563fadc1bfb8eb3607832c343b4f198fc13.

--- test/test_turing.jl
+++ test/test_turing.jl
@@ -1,11 +1,9 @@
-using Zygote
-
 @testset "Turing-gradient" begin
     target = Pigeons.toy_turing_unid_target()
 
     @show Threads.nthreads()
 
-    logz_mala = stepping_stone_pair(pigeons(; target, explorer = AutoMALA(adapt_pre_conditioning = false, default_autodiff_backend = :Zygote)))
+    logz_mala = stepping_stone_pair(pigeons(; target, explorer = AutoMALA(adapt_pre_conditioning = false)))
     logz_slicer = stepping_stone_pair(pigeons(; target, explorer = SliceSampler()))
 
     @test abs(logz_mala[1] - logz_slicer[1]) < 0.1
