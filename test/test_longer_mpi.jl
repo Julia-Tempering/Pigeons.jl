bedf25a081ce66f0c2d5972263197b25e2f517e7
Alexandre Bouchard <alexandre.bouchard@gmail.com>
2023-07-20 14:34:13 -0700

    Fix bug in earlier refactor

--- test/test_longer_mpi.jl
+++ test/test_longer_mpi.jl
@@ -2,14 +2,13 @@ include("supporting/mpi_test_utils.jl")
 
 @testset "Longer MPI" begin
     n_mpis = set_n_mpis_to_one_on_windows(4)
-    record = []
     pigeons(
         target = Pigeons.TestSwapper(0.5),
         n_rounds = 14,
         checked_round = 12,
         n_chains = 200,
         multithreaded = false,
-        record = recorder_builders,
+        record = [],
         checkpoint = true,
         on = ChildProcess(
                 n_local_mpi_processes = n_mpis,
