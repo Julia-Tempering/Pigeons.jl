22ad9c34f489964a1aa41cf4af9666a7fd86657d
Alexandre Bouchard <alexandre.bouchard@gmail.com>
2023-07-24 05:24:23 -0700

    Bug fix of toggle utility

--- docs/toggle.jl
+++ docs/toggle.jl
@@ -2,7 +2,7 @@ function toggle(on = true, except = "reference.md")
     src_path = "$(@__DIR__)/src"
     for md_file in readdir(src_path)
         if endswith(md_file, on ? "bu" : "md") && !endswith(md_file, r"reference.md|interfaces.md")
-            dest = replace(md_file, on ? ("bu" => "md") : ("md" => "bu")) 
+            dest = replace(md_file, on ? (".bu" => ".md") : (".md" => ".bu")) 
             mv("$src_path/$md_file", "$src_path/$dest")
         end
     end
