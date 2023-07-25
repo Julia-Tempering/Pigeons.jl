01cd21b5dd9862f04f6db24cfcab7bdb45ac65cb
Alexandre Bouchard <alexandre.bouchard@gmail.com>
2023-07-24 19:47:49 -0700

    Draft online, add configuration points

--- docs/src/output-online.md
+++ docs/src/output-online.md
@@ -2,3 +2,33 @@
 CurrentModule = Pigeons
 ```
 
+# Online (constant memory) statistics 
+
+When the dimensionality of a model is large and/or the 
+number of MCMC samples is large, the samples may not 
+fit in memory. 
+The most flexible way to deal with this situation is 
+to write sample to disk and process them one at the time, 
+as described in [the off-memory processing documentation](output-off-memory.html). 
+However, certain statistics can be computed using fixed 
+dimensional sufficient statistics yielding more 
+efficient algorithms. We describe this alternative here. 
+
+
+## Built-in online statistics: mean and variance 
+
+```example online
+using Pigeons
+using OnlineStats
+
+# example target: Binomial likelihood with parameter p = p1 * p2
+an_unidentifiable_model = Pigeons.toy_turing_unid_target()
+
+pt = pigeons(
+        target = an_unidentifiable_model, 
+        record = [online]
+    )
+
+using Statistics 
+mean(pt)
+```
--- src/recorders/OnlineStateRecorder.jl
+++ src/recorders/OnlineStateRecorder.jl
@@ -17,10 +17,16 @@ $SIGNATURES
 """
 Statistics.var(pt::PT, variable_name::Symbol = :singleton_variable) = get_statistic(pt, variable_name, Variance) 
 
+""" 
+$SIGNATURES
+"""
 get_statistic(pt::PT, variable_name::Symbol, t::Type{T}) where {T} = get_statistic(pt.reduced_recorders, variable_name, t)
+
+# Internal use for sampler adaptation
 get_transformed_statistic(reduced_recorders, variable_name::Symbol, t::Type{T}) where {T} = 
     get_statistic(reduced_recorders, variable_name, t, false)
-function get_statistic(reduced_recorders, variable_name::Symbol, ::Type{T}, original_param = true) where {T}
+
+    function get_statistic(reduced_recorders, variable_name::Symbol, ::Type{T}, original_param = true) where {T}
     recorder = original_param ? reduced_recorders.online : reduced_recorders._transformed_online
     key = Pair(variable_name, T)
     v = value(recorder.stats[key]) 
@@ -50,14 +56,32 @@ $SIGNATURES
 """
 continuous_variables(pt::PT) = continuous_variables(locals(pt.replicas)[1].state) 
 
-const STATS = [Mean, Variance]
+"""
+`OnlineStat` types to be computed when the [`online()`] 
+recorder is enabled. 
+"""
+const registered_online_types = [Mean, Variance]
+
+"""
+$SIGNATURES 
+
+Register an additional `OnlineStat` sub-types to be computed when the [`online()`] 
+recorder is enabled. 
+
+The provided type should have a zero-argument constructor. 
+"""
+function register_online_type(t::Type{T}) where {T <: OnlineStat{Number}}
+    if !(t in registered_online_types) 
+        push!(registered_online_types, t)
+    end
+end
 
 function record!(recorder::OnlineStateRecorder, state)
     if isempty(recorder.stats)
         initialize_online_state_recorder!(recorder.stats, state)
     end 
     for name in continuous_variables(state) 
-        for stat in STATS # NB: the more natural "for key in keys(recorder.stats)" leads to allocations in the inner loop
+        for stat in registered_online_types # NB: the more natural "for key in keys(recorder.stats)" leads to allocations in the inner loop
             key = Pair(name, stat)
             fit!(recorder.stats[key], variable(state, name))
         end
@@ -65,7 +89,7 @@ function record!(recorder::OnlineStateRecorder, state)
 end
 
 initialize_online_state_recorder!(stats, state) = 
-    for stat_type in STATS
+    for stat_type in registered_online_types
         initialize_online_state_recorder!(stats, state, stat_type)
     end 
 
