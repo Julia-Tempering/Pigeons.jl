40e56b93458a8214f0a4711872bcb7b9d2f13463
Documenter.jl <documenter@juliadocs.github.io>
2023-07-25 16:21:51 +0000

    build based on a7738c5

---
+++ dev/results/all/2023-07-25-16-15-45-6Fds0O68/.launch_script.jl
@@ -0,0 +1,15 @@
+using Serialization
+using Pigeons
+Pigeons.mpi_active_ref[] = true
+
+pt_arguments = 
+    try
+        Pigeons.deserialize_immutables!(raw"/home/runner/work/Pigeons.jl/Pigeons.jl/docs/build/results/all/2023-07-25-16-15-45-6Fds0O68/immutables.jls")
+        deserialize(raw"/home/runner/work/Pigeons.jl/Pigeons.jl/docs/build/results/all/2023-07-25-16-15-45-6Fds0O68/.pt_argument.jls")
+    catch e
+        println("Hint: probably missing dependencies, use the dependencies argument in MPI() or ChildProcess()")
+        rethrow(e)
+    end
+
+pt = PT(pt_arguments, exec_folder = raw"/home/runner/work/Pigeons.jl/Pigeons.jl/docs/build/results/all/2023-07-25-16-15-45-6Fds0O68")
+pigeons(pt)
