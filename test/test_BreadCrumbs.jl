e9bb0796e9dec0824f3cd6c8ff2ce401a9696309
Miguel Biron-Lattes <miguelbl9@gmail.com>
2023-07-28 13:01:14 -0700

    simplify BreadCrumbs test

--- test/test_BreadCrumbs.jl
+++ test/test_BreadCrumbs.jl
@@ -2,11 +2,7 @@ using MCMCChains
 
 @testset "Multivariate BreadCrumbs" begin
     function unid_log_potential(x; n_trials=100, n_successes=50) 
-        p1, p2 = x
-        if !(0 < p1 < 1) || !(0 < p2 < 1)
-            return eltype(x)(-Inf) 
-        end
-        p = p1 * p2
+        p = prod(x)
         return n_successes*log(p) + (n_trials-n_successes)*log1p(-p)
     end
     ref_dist = product_distribution(Uniform(), Uniform())
