a7579dfeb0997e965982a4b4ba70cee0ec17a3e1
Alexandre Bouchard <alexandre.bouchard@gmail.com>
2023-08-20 13:13:23 -0700

    Flattened names for variable_names in Turing

--- test/test_turing.jl
+++ test/test_turing.jl
@@ -1,3 +1,5 @@
+include("supporting/turing_models.jl")
+
 #= 
 The reason this test is excluded is described in 
 ADgradient() in TuringLogPotential.jl
@@ -12,3 +14,8 @@ ADgradient() in TuringLogPotential.jl
 
 #     @test abs(logz_mala[1] - logz_slicer[1]) < 0.1
 # end
+
+@testset "Turing-variable-names" begin
+    pt = pigeons(target = TuringLogPotential(model_with_vectors()), n_rounds = 2);
+    @test length(variable_names(pt)) == 4
+end
