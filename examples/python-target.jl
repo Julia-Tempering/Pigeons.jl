2e699202d879ad52850c78f415610527e9212c96
Miguel Biron-Lattes <miguelbl9@gmail.com>
2023-08-31 18:51:32 -0700

    full example working, almost no allocs, not so fast tho

--- examples/python-target.jl
+++ examples/python-target.jl
@@ -1,5 +1,7 @@
 include("../test/activate_test_env.jl")
 using PythonCall
+using MCMCChains
+using StatsPlots
 
 ###############################################################################
 # Pigeons for Bayesian inference for parameters of ODE coded in Python (with Numba)
@@ -20,6 +22,7 @@ py_make_fun_str = """
 # import modules
 import numpy as np
 import pandas as pd
+from numpy.linalg import norm
 from numba import njit
 from scipy.integrate import odeint
 
@@ -42,48 +45,55 @@ def rhs(X, t, theta):
     dy_dt = -gamma * y + delta * x * y
     return [dx_dt, dy_dt]
 
-# function that calculates residuals based on a given theta
-def ode_model_resid(theta):
-    return (
+# calculate the loglikelihood of the model
+def ode_model_loglik(jl_x):
+    x     = np.asarray(jl_x) # https://cjdoris.github.io/PythonCall.jl/stable/juliacall-reference/#juliacall.ArrayValue
+    theta = x[:-1]
+    ssq   = norm((
         data[["hare", "lynx"]] - odeint(func=rhs, y0=theta[-2:], t=data.year, args=(theta,))
-    ).values.flatten()
+    ).values)
+    sigma = x[-1:].item()
+    return -np.log(sigma) - ssq/(2*sigma*sigma)
 
 """
 pyexec(py_make_fun_str, Main)
-jl_ode_model_resid(t) = pyeval(Vector{Float64},"ode_model_resid(theta)", Main, (theta=t,))
+jl_ode_model_loglik(x) = pyeval(Float64,"ode_model_loglik(jl_x)", Main, (jl_x=x,))
 const theta_init = [0.52, 0.026, 0.84, 0.026, 34.0, 5.9]
-@assert jl_ode_model_resid(theta_init) isa Vector
-@time jl_ode_model_resid(theta_init) # 9k allocs!!
+@assert jl_ode_model_loglik([theta_init;1.]) isa Float64
+@time jl_ode_model_loglik([theta_init;1.]) # allocs down from 9k to 20, but only twice as fast
 
 # construct the necessary pigeons inputs
 reference_lp = DistributionLogPotential(
     product_distribution(
-        TruncatedNormal(theta_init[1],0.1 ,0.,Inf),
-        TruncatedNormal(theta_init[2],0.01,0.,Inf),
-        TruncatedNormal(theta_init[3],0.1 ,0.,Inf),
-        TruncatedNormal(theta_init[4],0.01,0.,Inf),
-        TruncatedNormal(theta_init[5],1.0 ,0.,Inf),
-        TruncatedNormal(theta_init[6],1.0 ,0.,Inf),
-        TruncatedNormal(0.,10.,0.,Inf)
+        TruncatedNormal(theta_init[1],0.1 ,0.,Inf), # alpha
+        TruncatedNormal(theta_init[2],0.01,0.,Inf), # beta
+        TruncatedNormal(theta_init[3],0.1 ,0.,Inf), # gamma
+        TruncatedNormal(theta_init[4],0.01,0.,Inf), # delta
+        TruncatedNormal(theta_init[5],1.0 ,0.,Inf), # xt0
+        TruncatedNormal(theta_init[6],1.0 ,0.,Inf), # yt0
+        TruncatedNormal(0.,10.,0.,Inf)              # sigma
     )
 );
 struct PyLogPotential{R}
     ref_lp::R
 end
 function (plp::PyLogPotential)(x)
-    prior = plp.ref_lp(x)
+    prior  = plp.ref_lp(x)
     isinf(prior) && return prior
-    theta = @view x[begin:(end-1)]
-    sigma = last(x)
-    errs  = pyeval(Vector{Float64},"ode_model_resid(theta)", Main, (theta=theta,))
-    prior + loglikelihood(Normal(0.,sigma), errs)
+    loglik = jl_ode_model_loglik(x)
+    prior + loglik
 end
 Pigeons.initialization(::PyLogPotential, ::AbstractRNG, ::Int) = [theta_init;3.0]
 
 # run pigeons
-# incredibly slow due to the huge number of allocs per likelihood eval
+# incredibly slow due to each likelihood eval takin ~2ms
 pt = pigeons(
-    target = PyLogPotential(reference_lp),
-    reference = reference_lp, 
-    record = record_online()
+    target    = PyLogPotential(reference_lp),
+    reference = reference_lp,
+    explorer  = SliceSampler(w=0.01, p=9), # keep max window = w * 2^p  small, ow odeint will complain a lot!
+    record    = [online;traces;record_default()],
+    n_rounds  = 8,
+    n_chains  = 3
 )
+samples = Chains(sample_array(pt), variable_names(pt))
+StatsPlots.plot(samples)
