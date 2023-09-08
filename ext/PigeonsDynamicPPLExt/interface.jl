7a2fcf5c9687e52ccb189047baa998a935663698
Paul Tiede <ptiede91@gmail.com>
2023-09-08 17:54:14 -0400

    Start move to extensions

---
+++ ext/PigeonsDynamicPPLExt/interface.jl
@@ -0,0 +1,69 @@
+(log_potential::TuringLogPotential)(vi) =
+    try
+        if log_potential.only_prior
+            DynamicPPL.logprior(log_potential.model, vi)
+        else
+            # Bug fix: avoiding now to break into prior and likelihood
+            #          calls, as it would add the log Jacobian twice.
+            DynamicPPL.logjoint(log_potential.model, vi)
+        end
+    catch e
+        (isa(e, DomainError) | isa(e, BoundsError)) ? -Inf : error("Unknown error in evaluation of the Turing log_potential.")
+    end
+
+"""
+$SIGNATURES
+Given a `DynamicPPL.Model` from Turing.jl, create a
+`TuringLogPotential` conforming both [`target`](@ref) and
+[`log_potential`](@ref).
+"""
+Pigeons.@provides target Pigeons.TuringLogPotential(model::DynamicPPL.Model) =
+    TuringLogPotential(model, false)
+
+function Pigeons.initialization(target::TuringLogPotential, rng::AbstractRNG, _::Int64)
+    result = DynamicPPL.VarInfo(rng, target.model, DynamicPPL.SampleFromPrior(), DynamicPPL.PriorContext())
+    DynamicPPL.link!!(result, DynamicPPL.SampleFromPrior(), target.model)
+    return result
+end
+Pigeons.initialization(target::TuringLogPotential) = Pigeons.initialization(target, SplittableRandom(1), 1)
+
+# At the moment, AutoMALA assumes a :singleton_variable structure
+# so use the SliceSampler.
+Pigeons.default_explorer(::TuringLogPotential) = SliceSampler()
+
+Pigeons.default_reference(target::TuringLogPotential) =
+    TuringLogPotential(target.model, true)
+
+function Pigeons.sample_iid!(log_potential::TuringLogPotential, replica, shared)
+    replica.state = Pigeons.initialization(log_potential, replica.rng, replica.replica_index)
+end
+
+
+LogDensityProblemsAD.dimension(log_potential::TuringLogPotential) = length(DynamicPPL.getall(Pigeons.initialization(log_potential)))
+function LogDensityProblemsAD.ADgradient(kind::Symbol, log_potential::TuringLogPotential, buffers::Pigeons.Augmentation)
+    @warn   """
+            We recommend using SliceSampler() for Turing models. If you have a large continuous model
+            consider using the BridgeStan. The Turing interface is still useful for models containing
+            both continuous and discrete variables.
+
+            Details:
+
+            - The LogDensityProblems interface seems to force us to keep two representations of the states,
+              one for the VariableInfo and one vector based. This is only partly implemented at the moment,
+              as a result we have the following limitations: (1) AutoMALA+Turing cannot use the
+              diagonal pre-conditioning at the moment. (2) AutoMALA+Turing only works if all variables are
+              continuous at the moment. Both could be addressed but since the autodiff is pretty slow at the
+              moment it seems low priority; the user can just rely on SliceSampler() at the moment.
+            - If the user has a fully continuous model, there is a good alternative: the Stan Bridge,
+              (which has much faster autodiff than Zygote, and Enzyme crashes on Turing at the time of writing).
+            - On some Turing models, gradient computation is non-deterministic,
+              see 4433584a044510bf9360e1e7191e59478496dc0b and associated CIs at
+              https://github.com/Julia-Tempering/Pigeons.jl/actions/runs/5550424683/jobs/10135522013
+              vs
+              https://github.com/Julia-Tempering/Pigeons.jl/actions/runs/5550424683/jobs/10135521940
+              (look for output of test_turing.jl)
+            """ maxlog=1
+    context = log_potential.only_prior ? DynamicPPL.PriorContext() : DynamicPPL.DefaultContext()
+    fct = DynamicPPL.LogDensityFunction(Pigeons.initialization(log_potential), log_potential.model, context)
+    return ADgradient(kind, fct)
+end
--- src/Pigeons.jl
+++ src/Pigeons.jl
@@ -2,15 +2,15 @@ module Pigeons
 
 import SplittableRandoms: SplittableRandom, split
 
-import MPI: Comm, Allreduce, Comm_rank, 
-            Isend, Irecv!, Recv!, COMM_WORLD, 
-            Comm_size, Comm_rank, Init, 
+import MPI: Comm, Allreduce, Comm_rank,
+            Isend, Irecv!, Recv!, COMM_WORLD,
+            Comm_size, Comm_rank, Init,
             Comm_dup, Request, Waitall,
-            RequestSet, mpiexec, Allreduce, 
+            RequestSet, mpiexec, Allreduce,
             Allgather, Comm_split, isend, recv,
-            bcast, tag_ub 
+            bcast, tag_ub
 
-     
+
 using Base: Forward
 using DataFrames
 using Distributions
@@ -25,7 +25,7 @@ using LinearAlgebra
 using SpecialFunctions
 using Serialization
 using ConcreteStructs
-using Random 
+using Random
 using Graphs
 using DataStructures
 using Preferences
@@ -38,33 +38,31 @@ using Statistics
 using RecipesBase
 using ZipFile
 using ForwardDiff
-using BridgeStan
-using LogDensityProblems 
+using LogDensityProblems
 using LogDensityProblemsAD
 
 import Serialization.serialize
 import Serialization.deserialize
 import Base.@kwdef
-import Base.show 
-import Base.print 
+import Base.show
+import Base.print
 import Base.Threads.@threads
 import OnlineStats._fit!
 import OnlineStats.value
 import OnlineStats._merge!
-import Random.rand! 
+import Random.rand!
 import Base.(==)
 import Base.keys
-import Statistics.mean 
+import Statistics.mean
 import Statistics.var
 import Base.merge
 
-import DynamicPPL
 
 const use_auto_exec_folder = "use_auto_exec_folder"
 
 include("includes.jl")
 
-export pigeons, Inputs, PT, 
+export pigeons, Inputs, PT,
     # for running jobs:
     ChildProcess, MPI,
     # references:
@@ -72,20 +70,34 @@ export pigeons, Inputs, PT,
     # targets:
     TuringLogPotential, StanLogPotential,
     # some examples
-    toy_mvn_target, toy_stan_target, 
+    toy_mvn_target, toy_stan_target,
     # post-processing helpers
     sample_array, variable_names,
     # recorders:
     index_process, swap_acceptance_pr, log_sum_ratio, online, round_trip, energy_ac1, traces, disk,
-    record_online, record_default, 
+    record_online, record_default,
     # utils to run on scheduler:
     Result, load, setup_mpi, queue_status, queue_ncpus_free, kill_job, watch,
     # getting information out of an execution:
     stepping_stone, n_tempered_restarts, n_round_trips, process_sample, get_sample,
     # variational references:
-    GaussianReference, 
-    # samplers 
+    GaussianReference,
+    # samplers
     SliceSampler, AutoMALA, Compose
+
+
+
+# This is required to make extensions work with < 1.9 versions
+if !isdefined(Base, :get_extension)
+    using Requires
+end
+
+@static if !isdefined(Base, :get_extension)
+    function __init__()
+        @require DynamicPPL = "366bfd00-2699-11ea-058f-f148b4cae6d8" include(joinpath(@__DIR__, "../ext/PigeonsDynamicPPLExt.jl"))
+        @require BridgeStan = "c88b6f0a-829e-4b0b-94b7-f06ab5908f5a" include(joinpath(@__DIR__, "../ext/PigeonsBridgeStanExt.jl"))
+    end
+end
+
+
 end # End module
-
-
