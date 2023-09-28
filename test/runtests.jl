263806d87d5bd7a21cda6a14200853e1e9682d99
Miguel Biron-Lattes <miguelbl9@gmail.com>
2023-09-28 15:29:37 -0700

    test in CI

--- test/runtests.jl
+++ test/runtests.jl
@@ -7,12 +7,10 @@ test_dir = @__DIR__
 project_root_dir = dirname(test_dir)
 @assert pathof(Pigeons) == joinpath(project_root_dir, "src", "Pigeons.jl") 
 
-# load all files starting with "test_"
-for test_name in filter(x -> startswith(x, "test_") && endswith(x, ".jl"), readdir(test_dir)) 
-    # organize output a little bit
-    println() # v otherwise can't tell what is running when it crashes in the middle
-    println("### Starting $test_name")
-             # + - yes, we need this horror, because we are dealing with a macro
-             # v 
-    @testset "$test_name" include(test_name)
+@testset "issue 141" begin
+    result = pigeons(
+        target = toy_mvn_target(100), 
+        checkpoint = true, 
+        on = ChildProcess(n_local_mpi_processes = 4)
+    )
 end
