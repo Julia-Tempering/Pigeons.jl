3f96805f2751588958c415edfc5305fea4258f0f
Miguel Biron-Lattes <miguelbl9@gmail.com>
2023-10-20 14:44:37 -0700

    rm probs args

--- src/explorers/Mix.jl
+++ src/explorers/Mix.jl
@@ -10,27 +10,16 @@ $FIELDS
     A tuple consisting of exploration kernels
     """
     explorers
-    """
-    The base number of steps (equivalently, momentum refreshments) between swaps.
-    This base number gets multiplied by `ceil(Int, dim^(exponent_n_refresh))`. 
-    """
-    categorical
 end
-function Mix(explorers::Tuple, probs::AbstractVector{<:Real})
-    @assert length(explorers) == length(probs)
-    Mix(explorers, Categorical(probs))
-end
-Mix(explorers::Tuple) = Mix(explorers, Categorical(length(explorers)))
-Mix(explorers...; probs=fill(inv(length(explorers)),length(explorers))) = 
-    Mix(Tuple(explorers),probs)
+Mix(explorers...) = Mix(Tuple(explorers))
+
 adapt_explorer(explorer::Mix, reduced_recorders, current_pt, new_tempering) = 
     Mix(
-        Tuple(adapt_explorer(e, reduced_recorders, current_pt, new_tempering) for e in explorer.explorers),
-        explorer.categorical
+        Tuple(adapt_explorer(e, reduced_recorders, current_pt, new_tempering) for e in explorer.explorers)
     )
 
 step!(explorer::Mix, replica, shared) = 
-    step!(explorer.explorers[rand(replica.rng, explorer.categorical)], replica, shared) 
+    step!(rand(replica.rng, explorer.explorers), replica, shared) 
 
 function explorer_recorder_builders(explorer::Mix)
     result = Function[]
