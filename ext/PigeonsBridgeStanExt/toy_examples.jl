8e87006196baa66f989466dab8b50936b72d4a44
Paul Tiede <ptiede91@gmail.com>
2023-10-27 15:33:38 -0400

    Fix toy examples

--- ext/PigeonsBridgeStanExt/toy_examples.jl
+++ ext/PigeonsBridgeStanExt/toy_examples.jl
@@ -31,10 +31,10 @@ Pigeons.stan_bernoulli(y = [0,1,0,0,0,0,0,0,0,1]) =
         Pigeons.json(; y, N = length(y))
     )
 
-Pigeons.stan_banana(dim = 9) =
+Pigeons.stan_banana(dim = 9, scale=1.0) =
     StanLogPotential(
         stan_example_path("banana.stan"),
-        Pigeons.json(; dim)
+        Pigeons.json(; dim, scale)
     )
 
 
