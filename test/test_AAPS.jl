e4557b961b6d314e8cf55ecf24418760446e78eb
Miguel Biron-Lattes <miguelbl9@gmail.com>
2023-10-30 16:19:14 -0700

    Update test_AAPS.jl

--- test/test_AAPS.jl
+++ test/test_AAPS.jl
@@ -5,6 +5,6 @@ if !is_windows_in_CI()
             target = Pigeons.stan_banana(1, 1.0), 
             explorer = AAPS(step_size = 2. ^(-4)), 
             n_chains = 1, n_rounds = 12, record = [traces])
-        @test abs(23-minimum(ess(Chains(sample_array(pt))).nt.ess)) < 1            
+        @test abs(7.5-minimum(ess(Chains(sample_array(pt))).nt.ess)) < 1            
     end
 end
