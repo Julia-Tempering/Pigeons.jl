009c9f74233d7f05e9b23013a6e1a43725ae3898
Alexandre Bouchard <bouchard@stat.ubc.ca>
2023-10-30 09:47:19 -0700

    Triple checking that the problem does not arise with a pure Julia target

--- test/test_turing.jl
+++ test/test_turing.jl
@@ -1,5 +1,17 @@
 include("supporting/turing_models.jl")
 
+
+@testset "Non-turing-gradient" begin
+    target = Pigeons.toy_mvn_target(2)
+
+    @show Threads.nthreads()
+
+    logz_mala = Pigeons.stepping_stone_pair(pigeons(; target, explorer = AutoMALA(preconditioner = Pigeons.IdentityPreconditioner())))
+    logz_slicer = Pigeons.stepping_stone_pair(pigeons(; target, explorer = SliceSampler()))
+
+    @test abs(logz_mala[1] - logz_slicer[1]) < 0.1
+end
+
 #= 
 The reason this test is excluded is described in 
 ADgradient() in TuringLogPotential.jl
