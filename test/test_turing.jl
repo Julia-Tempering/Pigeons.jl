9987e352650fe4f83e46a9bb2fae9fc60ef53df2
Alexandre Bouchard <bouchard@stat.ubc.ca>
2023-10-31 08:38:48 -0700

    Restore same order as before

--- test/test_turing.jl
+++ test/test_turing.jl
@@ -1,16 +1,7 @@
 include("supporting/turing_models.jl")
 
 
-@testset "Non-turing-gradient" begin
-    target = Pigeons.toy_mvn_target(2)
 
-    @show Threads.nthreads()
-
-    logz_mala = Pigeons.stepping_stone_pair(pigeons(; target, explorer = AutoMALA(preconditioner = Pigeons.IdentityPreconditioner())))
-    logz_slicer = Pigeons.stepping_stone_pair(pigeons(; target, explorer = SliceSampler()))
-
-    @test abs(logz_mala[1] - logz_slicer[1]) < 0.1
-end
 
 #= 
 The reason this test is excluded is described in 
@@ -30,4 +21,15 @@ end
 @testset "Turing-variable-names" begin
     pt = pigeons(target = TuringLogPotential(model_with_vectors()), n_rounds = 2);
     @test length(variable_names(pt)) == 4
+end
+
+@testset "Non-turing-gradient" begin
+    target = Pigeons.toy_mvn_target(2)
+
+    @show Threads.nthreads()
+
+    logz_mala = Pigeons.stepping_stone_pair(pigeons(; target, explorer = AutoMALA(preconditioner = Pigeons.IdentityPreconditioner())))
+    logz_slicer = Pigeons.stepping_stone_pair(pigeons(; target, explorer = SliceSampler()))
+
+    @test abs(logz_mala[1] - logz_slicer[1]) < 0.1
 end
