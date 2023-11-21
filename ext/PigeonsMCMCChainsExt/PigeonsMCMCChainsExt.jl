d1b2c78caccf30f26b6ac267a0281ed374a04b03
Alexandre Bouchard <bouchard@stat.ubc.ca>
2023-11-20 16:07:01 -0800

    Fix in ext?

--- ext/PigeonsMCMCChainsExt/PigeonsMCMCChainsExt.jl
+++ ext/PigeonsMCMCChainsExt/PigeonsMCMCChainsExt.jl
@@ -4,11 +4,9 @@ using Pigeons
 if isdefined(Base, :get_extension)
     using DocStringExtensions
     using MCMCChains
-    import MCMCChains.Chains
 else
     using ..DocStringExtensions
     using ..MCMCChains
-    import MCMCChains.Chains
 end
 
 MCMCChains.Chains(pt::PT) = Chains(sample_array(pt), variable_names(pt), Dict(:internals => [:log_density]))
