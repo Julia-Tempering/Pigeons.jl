8c54c01d1372da8f017911bb42c57c6a5e5549f7
Alexandre Bouchard <bouchard@stat.ubc.ca>
2023-11-25 11:23:33 -0800

    Fix preset

--- src/submission/presets.jl
+++ src/submission/presets.jl
@@ -12,7 +12,7 @@ UBC Sockeye cluster.
 """
 setup_mpi_sockeye(my_user_allocation_code) =
     setup_mpi(
-        submission_system = :pbs,
+        submission_system = :slurm,
         environment_modules = ["gcc", "openmpi", "git"],
         add_to_submission = ["#PBS -A $my_user_allocation_code"], 
         library_name = "/arc/software/spack-2023/opt/spack/linux-centos7-skylake_avx512/gcc-9.4.0/openmpi-4.1.1-d7o6cdvp67ngi5c5wdcw2qyjyseq3l3o/lib/libmpi"
