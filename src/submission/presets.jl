6dfe3432dbd7fbd81e576597ea32f8e4c5d24ae6
Alexandre Bouchard <bouchard@stat.ubc.ca>
2023-11-25 10:03:08 -0800

    Fix preset

--- src/submission/presets.jl
+++ src/submission/presets.jl
@@ -13,7 +13,7 @@ UBC Sockeye cluster.
 setup_mpi_sockeye(my_user_allocation_code) =
     setup_mpi(
         submission_system = :pbs,
-        environment_modules = ["git", "gcc", "intel-mkl", "openmpi"],
+        environment_modules = ["gcc", "openmpi", "git"],
         add_to_submission = ["#PBS -A $my_user_allocation_code"], 
         library_name = "/arc/software/spack-2023/opt/spack/linux-centos7-skylake_avx512/gcc-9.4.0/openmpi-4.1.1-d7o6cdvp67ngi5c5wdcw2qyjyseq3l3o/lib/libmpi"
     )
@@ -26,6 +26,6 @@ Compute Canada clusters.
 setup_mpi_compute_canada() = 
     setup_mpi(
         submission_system = :slurm,
-        environment_modules = ["gcc", "openmpi", "julia"],
-        library_name = "/arc/software/spack-2023/opt/spack/linux-centos7-skylake_avx512/gcc-9.4.0/openmpi-4.1.1-d7o6cdvp67ngi5c5wdcw2qyjyseq3l3o/lib/libmpi"
+        environment_modules = ["intel", "openmpi", "julia"],
+        library_name = library_name = "/cvmfs/soft.computecanada.ca/easybuild/software/2020/avx2/Compiler/intel2020/openmpi/4.0.3/lib/libmpi"
     )
