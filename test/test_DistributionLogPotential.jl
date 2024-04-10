9ca6cd58e32ac84e73002e27c001d02d15241ddc
Miguel Biron-Lattes <miguelbl9@gmail.com>
2024-04-10 15:23:21 -0700

    skip test on windows ci

--- test/test_DistributionLogPotential.jl
+++ test/test_DistributionLogPotential.jl
@@ -30,43 +30,45 @@ end
     @test abs(Pigeons.global_barrier(pt) - 3.15) < 0.1
 end
 
-@testset "DLP: Stan interface + iid sampling" begin
-    # recreate Fig. 6 in Ballnus et al. (2017)
+@static if !is_windows_in_CI()
+    @testset "DLP: Stan interface + iid sampling" begin
+        # recreate Fig. 6 in Ballnus et al. (2017)
 
-    # load data
-    dta_path = joinpath(dirname(@__DIR__), "examples", "data", "Ballnus_et_al_2017_M1a.csv")
-    dta = readdlm(dta_path, ',')
-    N   = size(dta, 1)
-    ts  = dta[:,1]
-    ys  = dta[:,2]
+        # load data
+        dta_path = joinpath(dirname(@__DIR__), "examples", "data", "Ballnus_et_al_2017_M1a.csv")
+        dta = readdlm(dta_path, ',')
+        N   = size(dta, 1)
+        ts  = dta[:,1]
+        ys  = dta[:,2]
 
-    # create model target and reference
-    model_file = joinpath(dirname(@__DIR__), "examples", "stan", "mRNA.stan")
-    mRNA_target = StanLogPotential(model_file, Pigeons.json(; N, ts, ys))
-    prior_ref = DistributionLogPotential(product_distribution(
-        Uniform(-2,1), Uniform(-5,5), Uniform(-5,5), Uniform(-5,5), Uniform(-2,2)
-    ))
+        # create model target and reference
+        model_file = joinpath(dirname(@__DIR__), "examples", "stan", "mRNA.stan")
+        mRNA_target = StanLogPotential(model_file, Pigeons.json(; N, ts, ys))
+        prior_ref = DistributionLogPotential(product_distribution(
+            Uniform(-2,1), Uniform(-5,5), Uniform(-5,5), Uniform(-5,5), Uniform(-2,2)
+        ))
 
-    # run
-    results = pigeons(
-        target = mRNA_target, 
-        reference = prior_ref, 
-        record = [round_trip; record_default()],
-        multithreaded = false,
-        n_chains = 15, # Λ ~ 7
-        n_rounds = 4,  # use >=16 to get close to figure in the paper
-        checkpoint = true,
-        on = ChildProcess(
-            n_local_mpi_processes = n_mpis,
-            n_threads = 1,
-            dependencies = [BridgeStan]
+        # run
+        results = pigeons(
+            target = mRNA_target, 
+            reference = prior_ref, 
+            record = [round_trip; record_default()],
+            multithreaded = false,
+            n_chains = 15, # Λ ~ 7
+            n_rounds = 4,  # use >=16 to get close to figure in the paper
+            checkpoint = true,
+            on = ChildProcess(
+                n_local_mpi_processes = n_mpis,
+                n_threads = 1,
+                dependencies = [BridgeStan]
+            )
         )
-    )
-    pt = Pigeons.load(results)
-    @test abs(Pigeons.global_barrier(pt) - 4.54) < 0.1
+        pt = Pigeons.load(results)
+        @test abs(Pigeons.global_barrier(pt) - 4.54) < 0.1
 
-    # using PairPlots, CairoMakie
-    # samples = Chains(Pigeons.load(pt))
-    # fig = pairplot(samples[:,1:5,:])
-    # save("myfigure.png", fig)
+        # using PairPlots, CairoMakie
+        # samples = Chains(Pigeons.load(pt))
+        # fig = pairplot(samples[:,1:5,:])
+        # save("myfigure.png", fig)
+    end
 end
