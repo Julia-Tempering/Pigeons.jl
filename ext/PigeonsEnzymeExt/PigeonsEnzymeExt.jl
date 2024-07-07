db482c9d5c095a3d65b7a45c034d74951ff1f9bf
Miguel Biron-Lattes <miguelbl9@gmail.com>
2024-07-07 14:23:41 -0700

    hack to get hidden type

--- ext/PigeonsEnzymeExt/PigeonsEnzymeExt.jl
+++ ext/PigeonsEnzymeExt/PigeonsEnzymeExt.jl
@@ -13,16 +13,16 @@ else
     using ..LogDensityProblemsAD
 end
 
-# adapted from LogDensityProblemsAD to use the Replica's buffer
 # TODO: currently, the concrete versions of ADGradientWrapper are defined only
 # in the extensions of LogDensityProblemsAD. Therefore, it is impossible to 
 # dispatch on them; see 
 #   https://github.com/tpapp/LogDensityProblemsAD.jl/issues/32
-# Since this (Enzyme) is our only extension using the ADGradientWrapper interface
-# it is ok to use this abstract type to dispatch. But problems will arise the minute
-# we want to implement another backend. So keep an eye on the above issue.
+# This is a HACK to extract that type 
+const EnzymeGradientLogDensity = Base.get_extension(LogDensityProblemsAD, :LogDensityProblemsADEnzymeExt).EnzymeGradientLogDensity
+
+# adapted from LogDensityProblemsAD to use the Replica's buffer
 function LogDensityProblems.logdensity_and_gradient(
-    b::Pigeons.BufferedAD{<:LogDensityProblemsAD.ADGradientWrapper}, # assuming Enzyme (FIXME)
+    b::Pigeons.BufferedAD{<:EnzymeGradientLogDensity},
     x::AbstractVector
     )
     ℓ = b.enclosed.ℓ
