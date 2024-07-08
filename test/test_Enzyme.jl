066aee2596fb2907f4a30f8ad2ee222a7be98338
Miguel Biron-Lattes <miguelbl9@gmail.com>
2024-07-08 11:54:52 -0700

    change name

--- test/test_Enzyme.jl
+++ test/test_Enzyme.jl
@@ -1,7 +1,7 @@
 using Enzyme
 
 @testset "autoMALA with Enzyme on custom Julia target" begin
-    struct MyLogPotential 
+    struct CustomUnidTarget 
         n_trials::Int
         n_successes::Int
     end
@@ -9,7 +9,7 @@ using Enzyme
     # NB: this is a constrained target but we haven't yet implemented constrained
     # sampling for autoMALA. Still, the purpose here is to check that the buffered
     # Enzyme implementation works, and for this the test is sufficient. 
-    function (log_potential::MyLogPotential)(x) 
+    function (log_potential::CustomUnidTarget)(x) 
         p1, p2 = x
         if !(0 < p1 < 1) || !(0 < p2 < 1)
             return -Inf64 
@@ -18,22 +18,22 @@ using Enzyme
         logpdf(Binomial(log_potential.n_trials, p), log_potential.n_successes)
     end
     
-    my_log_potential = MyLogPotential(100, 50)
+    my_log_potential = CustomUnidTarget(100, 50)
     
-    Pigeons.initialization(::MyLogPotential, ::AbstractRNG, ::Int) = [0.5, 0.5]
+    Pigeons.initialization(::CustomUnidTarget, ::AbstractRNG, ::Int) = [0.5, 0.5]
     
-    function Pigeons.sample_iid!(::MyLogPotential, replica, shared)
+    function Pigeons.sample_iid!(::CustomUnidTarget, replica, shared)
         state = replica.state 
         rng = replica.rng 
         rand!(rng, state)
     end
     
-    LogDensityProblems.dimension(lp::MyLogPotential) = 2
-    LogDensityProblems.logdensity(lp::MyLogPotential, x) = lp(x)
+    LogDensityProblems.dimension(lp::CustomUnidTarget) = 2
+    LogDensityProblems.logdensity(lp::CustomUnidTarget, x) = lp(x)
     
     pt = pigeons(
-            target = MyLogPotential(100, 50), 
-            reference = MyLogPotential(0, 0), 
+            target = CustomUnidTarget(100, 50), 
+            reference = CustomUnidTarget(0, 0), 
             n_chains = 4,
             explorer = AutoMALA(default_autodiff_backend = :Enzyme) 
     )
