f4a3993ecebde1b4360baf897ffce683dd76276c
Alexandre Bouchard <bouchard@stat.ubc.ca>
2025-03-28 15:54:02 -0700

    Prototype benchmark code

---
+++ test/benchmark.jl
@@ -0,0 +1,24 @@
+# usage: julia test/benchmark.jl > test_results.csv
+
+
+using Pkg
+script_dir = @__DIR__
+Pkg.activate(script_dir)
+parent_dir = dirname(script_dir)
+Pkg.develop(PackageSpec(path=parent_dir))
+
+println("test_name, time (s), memory (bytes)")
+
+benchmark(timing, test_name::String) = println("$test_name, $(timing.time), $(timing.bytes)")
+
+function benchmark(lambda::Function, test_name::String)
+    benchmark(@timed(lambda()), test_name)
+    benchmark(@timed(lambda()), test_name * " (second call)")
+end
+
+include_timing = @timed using Pigeons 
+benchmark(include_timing, "using Pigeons")
+
+benchmark("mvn-1000") do 
+    pigeons(target = toy_mvn_target(1000), show_report = false)
+end
