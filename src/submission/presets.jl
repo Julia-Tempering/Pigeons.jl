d63ae3647ecfa3d6dca9950de1f407bf698903da
Alexandre Bouchard <bouchard@stat.ubc.ca>
2025-04-02 15:10:35 -0700

    Update Sockeye preset

--- src/submission/presets.jl
+++ src/submission/presets.jl
@@ -18,7 +18,7 @@ setup_mpi_sockeye(my_user_allocation_code) =
             "#SBATCH -A $my_user_allocation_code"
             "#SBATCH --nodes=1-10000"  # required by cluster
         ], 
-        library_name = "/arc/software/spack-2023/opt/spack/linux-centos7-skylake_avx512/gcc-9.4.0/openmpi-4.1.1-d7o6cdvp67ngi5c5wdcw2qyjyseq3l3o/lib/libmpi"
+        library_name = "/arc/software/spack-2024/opt/spack/linux-rocky9-skylake_avx512/gcc-9.4.0/openmpi-4.1.1-7c42tpwdhfycqa3vdhit4mspyk6a32nn/lib/libmpi"
     )
 
 """ 
