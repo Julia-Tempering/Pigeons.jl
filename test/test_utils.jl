338f3d21b53d21bb0aad06ac2b8ea42f3e039261
Alexandre Bouchard <bouchard@stat.ubc.ca>
2025-04-17 16:01:37 -0700

    Remove part of test
    In CI, Base.active_project() does not give @vXXX.YYY

--- test/test_utils.jl
+++ test/test_utils.jl
@@ -1,11 +1,3 @@
 @testset "Test is_default_env" begin
     @test !Pigeons.is_default_env()
-    original_project = Base.active_project()
-    try
-        Pkg.activate() # Activate default
-        @show Base.active_project()
-        @test Pigeons.is_default_env()
-    finally
-        Pkg.activate(original_project) 
-    end
 end
