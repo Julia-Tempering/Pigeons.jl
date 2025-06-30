7cf863b54a07e65f512f08231e8792e9ab5cb555
Jack <therealjackwu@gmail.com>
2025-06-30 12:51:15 -0700

    added greedy search algorithm

--- src/variational/TreeReference.jl
+++ src/variational/TreeReference.jl
@@ -2,23 +2,26 @@
 A Gaussian tree variational reference
 """
 
+import Pkg
+Pkg.add("DataStructures")
+using DataStructures
 
 @kwdef mutable struct TreeReference
-    edges::Vector{Tuple{Symbol, Symbol, Any}} = Vector{Tuple{Symbol, Symbol, Any}}()
-    means::Dict{Symbol, Any} = Dict{Symbol, Any}()
-    standard_deviations::Dict{Symbol, Any} = Dict{Symbol, Any}()
-    first_tuning_round::Int = 6 
-    num_nodes::Int = 0
+    edge_set::Vector{Tuple{Float64, Symbol, Symbol}} = Vector{Tuple{Float64, Symbol, Symbol}}()
+    mean::Dict{Symbol, Any} = Dict{Symbol, Any}()
+    standard_deviation::Dict{Symbol, Any} = Dict{Symbol, Any}()
+    first_tuning_round::Int = 6
 
-    function TreeReference(edges, means, standard_deviations, first_tuning_round, num_nodes)
-        @assert length(edges)==num_nodes-1 || length(edges)==(num_nodes*(num_nodes-1))/2
+    function TreeReference(edge_set, mean, standard_deviation, first_tuning_round)
+        dim = length(mean)
+        @assert length(edge_set)==dim-1 || length(edge_set)==(dim*(dim-1))/2
         @assert first_tuning_round ≥ 1
-        new(edges, means, standard_deviations, first_tuning_round, num_nodes)
+        new(edge_set, mean, standard_deviation, first_tuning_round)
     end
 end
 
 
-dim(variational::TreeReference) = variational.num_nodes
+dim(variational::TreeReference) = length(variational.mean)
 function activate_variational(variational::TreeReference, iterators::Iterators)
     iterators.round ≥ variational.first_tuning_round ? true : false
 end
@@ -36,17 +39,40 @@ end
 function compute_mutual_info()
 end
 
-#TODO
-function prims_spanning_tree()
+
+function directed_max_tree(adjacency_list, root)
+    total_number_of_nodes = length(keys(adjacency_list))
+    mst = Vector{Tuple{Symbol, Symbol}}()
+    pq = BinaryMaxHeap{Tuple{Float64, Symbol, Symbol}}()
+    visited_nodes = Set{Symbol}()
+    push!(visited_nodes, root)
+
+    for edge in adjacency_list[root]
+        push!(pq, edge)
+    end
+    
+    while !isempty(pq) && length(mst)<total_number_of_nodes-1
+        edge = pop!(pq)
+
+        if !(edge[3] in visited_nodes)
+            push!(visited_nodes, edge[3])
+            push!(mst, (edge[2], edge[3]))
+
+            for new_edge in adjacency_list[edge[3]]
+                if !(new_edge[3] in visited_nodes)
+                    push!(pq, new_edge)
+                end
+            end
+        end
+    end
+    @assert length(mst) == total_number_of_nodes-1 
+    return mst
 end
 
 
 
 #TODO
 function sample_iid!(variational::TreeReference, replica, shared)
-    for var_name in continuous_variables(replica.state)
-        for i in eachindex(variational.mean[var_name])
-            val = randn(replica.rng) * variational_standard
 end
 
 
@@ -55,13 +81,13 @@ function (variational::TreeReference)(state)
 
     marginal_var_name = continuous_variables(state)[1]
     marginal_state = variable(state, marginal_var_name)
-    marginal_mean = variational.means[marginal_var_name]
-    marginal_standard_deviation = variational.standard_deviations[marginal_var_name]
+    marginal_mean = variational.mean[marginal_var_name]
+    marginal_standard_deviation = variational.standard_deviation[marginal_var_name]
     log_pdf += logpdf(Normal(marginal_mean, marginal_standard_deviation), marginal_state)
 
-     for edges in variational.edges
-        parent_var_name = edges[1]
-        child_var_name = edges[2]
+     for edge in variational.edge_set
+        parent_var_name = edge[1]
+        child_var_name = edge[2]
 
         state_at_parent = variable(state, parent_var_name)
         state_at_child = variable(state, child_var_name)
@@ -73,10 +99,10 @@ end
 
 
 function tree_logdensity(variational::TreeReference, child_var_name, parent_var_name, state_at_child, state_at_parent)
-    child_mean = variational.means[child_var_name]
-    parent_mean = variational.means[parent_var_name]
-    child_standard_deviation = variational.standard_deviations[child_var_name]
-    parent_standard_deviation = variational.standard_deviations[child_var_name]
+    child_mean = variational.mean[child_var_name]
+    parent_mean = variational.mean[parent_var_name]
+    child_standard_deviation = variational.standard_deviation[child_var_name]
+    parent_standard_deviation = variational.standard_deviation[child_var_name]
 
     rho = get_rho(parent_var_name, child_var_name)
 
