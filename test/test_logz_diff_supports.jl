7f3cad7a66ee2aecc45bc0de13f341d73bbd9f33
Alexandre Bouchard <bouchard@stat.ubc.ca>
2025-08-11 09:32:36 -0700

    Add working test cases

--- test/test_logz_diff_supports.jl
+++ test/test_logz_diff_supports.jl
@@ -1,4 +1,4 @@
 @testset "Log z with different support for target and ref" begin
-
-    pt = pigeons(target = DistributionLogPotential(Uniform(-2, 2)), reference = DistributionLogPotential(Normal(0, 1)), n_rounds = 14)
+    pt = pigeons(target = DistributionLogPotential(Uniform(-2, 2)), reference = DistributionLogPotential(Uniform(0, 2)), n_rounds = 10, n_chains = 4)
+    pt = pigeons(target = DistributionLogPotential(Uniform(0, 2)), reference = DistributionLogPotential(Uniform(-2, 2)), n_rounds = 10, n_chains = 4)
 end
