9568b4fe6f926a114c904b15ad89656357a4f000
junsongt <junsongt@student.ubc.ca>
2026-01-18 02:41:41 -0800

    initialize manifold sampler script

---
+++ src/explorers/ManifoldSampler.jl
@@ -0,0 +1,22 @@
+"""
+$SIGNATURES
+
+Manifold sampling Algorithm with fixed/automatic step size selection.
+
+
+$FIELDS
+
+
+"""
+
+
+@kwdef struct ManifoldSampler
+
+
+end
+
+
+function manifold_sample!()
+
+    return
+end
