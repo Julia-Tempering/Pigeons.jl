844b09108c43672708251ed607e00ae34d5ba4e3
junsongt <junsongt@student.ubc.ca>
2026-08-21 14:17:57 -0700

    delibrately mess up the test to see if CI jumps over the failed ones

--- test/test_AAPS.jl
+++ test/test_AAPS.jl
@@ -1,11 +1,11 @@
 
 @testset "AAPS" begin
-    pt = pigeons(; 
-            target = toy_mvn_target(2), 
-            n_chains = 2, 
-            explorer = AAPS(),
-            record = [Pigeons.online], 
-            n_rounds = 10);
+    pt = pigeons(;
+        target=toy_mvn_target(2),
+        n_chains=2,
+        explorer=AAPS(),
+        record=[Pigeons.online],
+        n_rounds=10);
     for var_name in Pigeons.continuous_variables(pt)
         m = mean(pt, var_name)
         for i in 1:2  # not eachindex(v) as we skip :log_density
@@ -13,8 +13,9 @@
         end
         v = var(pt, var_name)
         for i in 1:2  # not eachindex(v) as we skip :log_density
-            @test abs(v[i] - 0.1) < 0.03
+            # @test abs(v[i] - 0.1) < 0.03
+            @test abs(v[i] - 0.1) > 0.03 # delibrately mess up the test to see if CI jumps over the failed ones
         end
     end
-            
+
 end
--- test/test_apis.jl
+++ test/test_apis.jl
@@ -1,20 +1,22 @@
 @testset "APIs" begin
-    r = pigeons(target = toy_mvn_target(1), checkpoint = true, on = ChildProcess())
+    r = pigeons(target=toy_mvn_target(1), checkpoint=true, on=ChildProcess())
     # less often used APIs: specify the path (string) to a checkpoint
     pigeons(r.exec_folder)
     # ... and to run on child
     pigeons(r.exec_folder, ChildProcess())
 
     # test with no recorders 
-    pigeons(target = toy_mvn_target(1), record = [])
+    pigeons(target=toy_mvn_target(1), record=[])
 end
 
-@testset "Reports" begin 
-    pt = pigeons(target = toy_mvn_target(1))
+@testset "Reports" begin
+    pt = pigeons(target=toy_mvn_target(1))
 
     swaps_data = pt.shared.reports.swap_prs
-    @test size(swaps_data)[1] == 10 * 9
+    # @test size(swaps_data)[1] == 10 * 9
+    @test size(swaps_data)[1] == 9 # delibrately mess up the test to see if CI jumps over the failed ones
 
     univ = pt.shared.reports.summary
-    @test size(univ)[1] == 10
+    # @test size(univ)[1] == 10
+    @test size(univ)[1] == 1 # delibrately mess up the test to see if CI jumps over the failed ones
 end
